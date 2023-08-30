bb113103bcb3822cf8a1000ad6743c8f88146f1b
Jay Lorch <lorch@microsoft.com>
2023-08-29 18:09:20 -0700

    Allow pmemlog to be used as a library

---
+++ pmemlog/src/lib.rs
@@ -0,0 +1,12 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+#![allow(non_upper_case_globals)]
+#![allow(unused_variables)]
+
+pub mod infinitelog_t;
+pub mod logimpl_v;
+pub mod main_t;
+pub mod math;
+pub mod pmemmock_t;
+pub mod pmemspec_t;
+pub mod sccf;
