59fd4c2b3f574804dad9866ced80ad89443573ca
hayley-leblanc <hleblanc@utexas.edu>
2024-05-07 10:10:40 -0500

    Durable list spec

--- storage_node/src/kv/durable/durablelist/durablelistspec_t.rs
+++ storage_node/src/kv/durable/durablelist/durablelistspec_t.rs
@@ -1,3 +1,4 @@
+use crate::kv::kvimpl_t::*;
 use crate::pmem::wrpm_v::*;
 use builtin::*;
 use builtin_macros::*;
@@ -26,8 +27,107 @@ verus! {
 
     // TODO: think about what this should actually be, might need a few layers
     #[verifier::reject_recursive_types(K)]
-    pub struct DurableListView<K, L>
+    pub struct DurableListView<K, L, E>
     {
         list: Map<K, Seq<DurableListElementView<L>>>,
+        _phantom: Option<E>
+    }
+
+    impl<K, L, E> DurableListView<K, L, E>
+        where
+            K: std::fmt::Debug,
+            E: std::fmt::Debug
+    {
+        pub closed spec fn spec_index(self, key: K) -> Option<Seq<DurableListElementView<L>>>
+        {
+            if self.list.contains_key(key) {
+                Some(self.list[key])
+            } else {
+                None
+            }
+        }
+
+        pub closed spec fn insert_key(self, key: K) -> Result<Self, KvError<K, E>>
+        {
+            if self.list.contains_key(key) {
+                Err(KvError::KeyAlreadyExists)
+            } else {
+                Ok(Self {
+                    list: self.list.insert(key, Seq::empty()),
+                    _phantom: None
+                })
+            }
+        }
+
+        pub closed spec fn insert_list_element(
+            self,
+            key: K,
+            crc: u64,
+            list_element: L,
+            index: int
+        ) -> Result<Self, KvError<K, E>>
+        {
+            if !self.list.contains_key(key) {
+                Err(KvError::KeyNotFound)
+            } else if index < 0 || index > self.list[key].len() {
+                Err(KvError::IndexOutOfRange)
+            } else {
+                let new_list = self.list[key].update(index, DurableListElementView { crc, list_element });
+                Ok(Self {
+                    list: self.list.insert(key, new_list),
+                    _phantom: None
+                })
+            }
+        }
+
+        pub closed spec fn append_list_element(
+            self,
+            key: K,
+            crc: u64,
+            list_element: L
+        ) -> Result<Self, KvError<K, E>>
+        {
+            if !self.list.contains_key(key) {
+                Err(KvError::KeyNotFound)
+            } else {
+                let new_list = self.list[key].push(DurableListElementView { crc, list_element });
+                Ok(Self {
+                    list: self.list.insert(key, new_list),
+                    _phantom: None
+                })
+            }
+        }
+
+        pub closed spec fn remove_key(
+            self,
+            key: K
+        ) -> Result<Self, KvError<K, E>>
+        {
+            if !self.list.contains_key(key) {
+                Err(KvError::KeyNotFound)
+            } else {
+                Ok(Self {
+                    list: self.list.remove(key),
+                    _phantom: None
+                })
+            }
+        }
+
+        pub closed spec fn trim_list(
+            self,
+            key: K,
+            trim_length: int
+        ) -> Result<Self, KvError<K, E>>
+        {
+            if !self.list.contains_key(key) {
+                Err(KvError::KeyNotFound)
+            } else {
+                let new_list = self.list[key].subrange(trim_length, self.list[key].len() as int);
+                Ok(Self {
+                    list: self.list.insert(key, new_list),
+                    _phantom: None
+                })
+            }
+        }
     }
 }
