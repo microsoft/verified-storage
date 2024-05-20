e59226fae0fe9411de151c59d5e68d2ff284e626
hayley-leblanc <hleblanc@utexas.edu>
2024-05-20 16:05:27 -0700

    Remove metadata table from list start and setup fn

--- storage_node/src/kv/durable/durablelist/durablelistimpl_v.rs
+++ storage_node/src/kv/durable/durablelist/durablelistimpl_v.rs
@@ -17,7 +17,7 @@ use vstd::prelude::*;
 use vstd::bytes::*;
 
 verus! {
-    pub const NUM_DURABLE_LIST_REGIONS: u64 = 2;
+    pub const NUM_DURABLE_LIST_REGIONS: u64 = 1;
 
     #[verifier::reject_recursive_types(K)]
     pub struct DurableList<K, L, E>
@@ -293,7 +293,7 @@ verus! {
                 }),
                 L::spec_serialized_len() + CRC_SIZE < u32::MAX, // serialized_len is u64, but we store it in a u32 here
                 // TODO: just pass it in as a u32
-                node_size < u32::MAX
+                0 < node_size <= u32::MAX
             ensures
                 pm_regions.inv(),
                 pm_regions@.no_outstanding_writes(),
@@ -311,29 +311,19 @@ verus! {
             // one for the metadata table and one for the node region
             let region_sizes = get_region_sizes(pm_regions);
             let num_regions = region_sizes.len();
-            if num_regions < 2 {
-                return Err(KvError::TooFewRegions{ required: 2, actual: num_regions });
+            if num_regions < 1 {
+                return Err(KvError::TooFewRegions{ required: 1, actual: num_regions });
             } else if num_regions > 2 {
-                return Err(KvError::TooManyRegions{ required: 2, actual: num_regions });
+                return Err(KvError::TooManyRegions{ required: 1, actual: num_regions });
             }
 
             // check that the regions the caller passed in are sufficiently large
-            let table_region_size = region_sizes[0];
-            let node_region_size = region_sizes[1];
-            let metadata_size = ListEntryMetadata::serialized_len();
-            let metadata_slot_size = metadata_size + CRC_SIZE + K::serialized_len() + CDB_SIZE;
+            let node_region_size = region_sizes[0];
 
             let list_element_size = L::serialized_len();
             let list_element_slot_size = list_element_size + CRC_SIZE;
 
-            // check that the table region is large enough -- needs at least as many slots as keys
-            if ABSOLUTE_POS_OF_METADATA_TABLE + (metadata_slot_size * num_keys as u64) > table_region_size {
-                let required = (ABSOLUTE_POS_OF_METADATA_TABLE + (metadata_slot_size * num_keys as u64)) as usize;
-                let actual = table_region_size as usize;
-                return Err(KvError::RegionTooSmall{required, actual});
-            }
-
-            // check that the table region is large enough -- needs to fit at least one node
+            // region needs to fit at least one node
             let required_node_region_size = ABSOLUTE_POS_OF_LIST_REGION_NODE_START + node_size as u64;
             if required_node_region_size > node_region_size {
                 let required = required_node_region_size as usize;
@@ -355,11 +345,12 @@ verus! {
         pub exec fn start<PM>(
             wrpm_regions: &mut WriteRestrictedPersistentMemoryRegions<TrustedListPermission, PM>,
             list_id: u128,
+            record_metadata: &GlobalListMetadata,
             Tracked(perm): Tracked<&TrustedListPermission>,
             Ghost(state): Ghost<DurableListView<K, L, E>>
         ) -> (result: Result<Self, KvError<K, E>>)
             where
-                PM: PersistentMemoryRegions
+                PM: PersistentMemoryRegions,
             requires
                 old(wrpm_regions).inv(),
                 ({
@@ -380,36 +371,23 @@ verus! {
             let pm_regions = wrpm_regions.get_pm_regions_ref();
             let region_sizes = get_region_sizes(pm_regions);
             let num_regions = region_sizes.len();
-            if num_regions < 2 {
-                return Err(KvError::TooFewRegions{ required: 2, actual: num_regions });
-            } else if num_regions > 2 {
-                return Err(KvError::TooManyRegions{ required: 2, actual: num_regions });
+            if num_regions < 1 {
+                return Err(KvError::TooFewRegions{ required: 1, actual: num_regions });
+            } else if num_regions > 1 {
+                return Err(KvError::TooManyRegions{ required: 1, actual: num_regions });
             }
-            // TODO: check that the regions are large enough to store metadata before we
-            // attempt to read them
 
             // Read the metadata headers for both regions
-            let list_metadata_table = Self::read_table_metadata(pm_regions, list_id)?;
             let list_region_metadata = Self::read_list_region_header(pm_regions, list_id)?;
 
             // check that the regions the caller passed in are sufficiently large
-            let table_region_size = region_sizes[0];
-            let node_region_size = region_sizes[1];
-            let metadata_size = ListEntryMetadata::serialized_len();
-            let metadata_slot_size = metadata_size + CRC_SIZE + K::serialized_len() + CDB_SIZE;
+            let node_region_size = region_sizes[0];
 
             let list_element_size = L::serialized_len();
             let list_element_slot_size = list_element_size + CRC_SIZE;
 
-            // check that the table region is large enough -- needs at least as many slots as keys
-            if ABSOLUTE_POS_OF_METADATA_TABLE + (metadata_slot_size * list_metadata_table.num_keys) > table_region_size {
-                let required = (ABSOLUTE_POS_OF_METADATA_TABLE + (metadata_slot_size * list_metadata_table.num_keys)) as usize;
-                let actual = table_region_size as usize;
-                return Err(KvError::RegionTooSmall{required, actual});
-            }
-
-            // check that the table region is large enough -- needs to fit at least one node
-            let required_node_region_size = ABSOLUTE_POS_OF_LIST_REGION_NODE_START + list_metadata_table.node_size as u64;
+            // region needs to fit at least one node
+            let required_node_region_size = ABSOLUTE_POS_OF_LIST_REGION_NODE_START + record_metadata.node_size as u64;
             if required_node_region_size > node_region_size {
                 let required = required_node_region_size as usize;
                 let actual = node_region_size as usize;
@@ -424,50 +402,10 @@ verus! {
             // separate vector to help traverse the lists and fill in list_nodes_in_use
             let mut list_node_region_stack: Vec<u64> = Vec::new();
 
-            // // construct allocator for the metadata table
-            // let mut metadata_table_free_list: Vec<u64> = Vec::with_capacity(list_metadata_table.num_keys as usize);
-            // for index in 0..list_metadata_table.num_keys {
-            //     assume(false);
-            //     let metadata_slot_offset = ABSOLUTE_POS_OF_METADATA_TABLE + index * metadata_slot_size;
-            //     let cdb_addr = metadata_slot_offset + RELATIVE_POS_OF_VALID_CDB;
-            //     let val: &u64 = pm_regions.read_and_deserialize(0, metadata_slot_offset + cdb_addr);
-            //     match check_cdb(&val, Ghost(mem),
-            //             Ghost(pm_regions.constants().impervious_to_corruption),
-            //             Ghost(cdb_addr)) {
-            //         Some(false) => metadata_table_free_list.push(index),
-            //         Some(true) => {
-            //             // We construct the list node region allocator and return metadata
-            //             // about list structures to the caller for indexing at the same time
-            //             // TODO: construct info to send to the volatile index once you have a better
-            //             // idea of what it will need
-            //             // TODO: move the read+check CRC logic into its own function
-            //             let crc_addr = metadata_slot_offset + RELATIVE_POS_OF_ENTRY_METADATA_CRC;
-            //             let entry_addr = metadata_slot_offset + RELATIVE_POS_OF_ENTRY_METADATA;
-            //             let key_addr = metadata_slot_offset + RELATIVE_POS_OF_ENTRY_KEY;
-            //             let entry_crc: &u64 = pm_regions.read_and_deserialize(0, crc_addr);
-            //             let entry: &ListEntryMetadata = pm_regions.read_and_deserialize(0, entry_addr);
-            //             let key: &K = pm_regions.read_and_deserialize(0, key_addr);
-            //             if !check_crc_two_deserialized(entry, key, entry_crc, Ghost(mem),
-            //                     Ghost(pm_regions.constants().impervious_to_corruption),
-            //                     Ghost(entry_addr),
-            //                     Ghost(key_addr),
-            //                     Ghost(crc_addr))
-            //             {
-            //                 return Err(KvError::CRCMismatch);
-            //             } else {
-            //                 // TODO: if the CRC check passes, add the information to the structure
-            //                 // we return for the volatile index to use
-            //                 list_node_region_stack.push(entry.get_head());
-            //             }
-            //         },
-            //         None => return Err(KvError::CRCMismatch)
-            //     }
-            // }
-
             // construct allocator for the list node region
             // we need to use two vectors for this -- one as a stack for traversal of the lists,
             // and one to record which nodes are in use
-            let ghost mem1 = pm_regions@[1].committed();
+            let ghost mem1 = pm_regions@[0].committed();
             while list_node_region_stack.len() != 0 {
                 assume(false);
                 let current_index = list_node_region_stack.pop().unwrap();
@@ -476,7 +414,7 @@ verus! {
                 // read the node, check its CRC; if it's fine, push its next
                 // pointer onto the stack
                 let list_node_offset = ABSOLUTE_POS_OF_LIST_REGION_NODE_START +
-                    current_index * (list_metadata_table.node_size as u64);
+                    current_index * (record_metadata.node_size as u64);
                 let ptr_addr = list_node_offset + RELATIVE_POS_OF_NEXT_POINTER;
                 let crc_addr = list_node_offset + RELATIVE_POS_OF_LIST_NODE_CRC;
                 let ptr_serialized_len = u64::serialized_len();
@@ -676,64 +614,31 @@ verus! {
             requires
                 old(pm_regions).inv(),
                 old(pm_regions)@.no_outstanding_writes(),
-                old(pm_regions)@.len() == 2,
+                old(pm_regions)@.len() == 1,
                 L::spec_serialized_len() + CRC_SIZE < u32::MAX, // serialized_len is u64, but we store it in a u32 here
-                ({
-                    // the first region is large enough to be the metadata table
-                    let metadata_size = ListEntryMetadata::spec_serialized_len();
-                    let metadata_slot_size = metadata_size + CRC_SIZE + K::spec_serialized_len() + CDB_SIZE;
-                    old(pm_regions)@[0].len() >= ABSOLUTE_POS_OF_METADATA_TABLE + (metadata_slot_size * num_keys)
-                }),
                 // the second region is large enough for at least one node
-                old(pm_regions)@[1].len() >= ABSOLUTE_POS_OF_LIST_REGION_NODE_START + node_size,
-                node_size <= u32::MAX,
+                old(pm_regions)@[0].len() >= ABSOLUTE_POS_OF_LIST_REGION_NODE_START + node_size,
+                0 < node_size <= u32::MAX,
             ensures
                 pm_regions.inv(),
                 // TODO
         {
-            // 1) Handle metadata table
-            // Initialize metadata table header and compute its CRC
-            let element_size = L::serialized_len() + CRC_SIZE;
-            let metadata_table_header = GlobalListMetadata {
-                element_size: (L::serialized_len() + CRC_SIZE) as u32,
-                node_size: node_size,
-                num_keys: num_keys as u64,
-                version_number: LIST_METADATA_VERSION_NUMBER,
-                _padding: 0,
-                program_guid: METADATA_TABLE_PROGRAM_GUID
-            };
-            let metadata_table_header_crc = calculate_crc(&metadata_table_header);
-
             assume(false);
-
-            pm_regions.serialize_and_write(0, ABSOLUTE_POS_OF_METADATA_HEADER, &metadata_table_header);
-            pm_regions.serialize_and_write(0, ABSOLUTE_POS_OF_HEADER_CRC, &metadata_table_header_crc);
-
-            let metadata_header_entry_slot_size = CDB_SIZE + CRC_SIZE + LENGTH_OF_ENTRY_METADATA_MINUS_KEY + K::serialized_len();
-            // invalidate all metadata table entries so that we can scan it and build the allocator
-            for index in 0..num_keys
-                // TODO: invariant
-            {
-                assume(false);
-                let entry_slot_offset = ABSOLUTE_POS_OF_METADATA_TABLE + index * metadata_header_entry_slot_size;
-                pm_regions.serialize_and_write(0, entry_slot_offset, &CDB_FALSE);
-            }
-
-            // 2) Handle list node region
+            // Handle list node region
             // Initialize list region header and compute its CRC
             let region_sizes = get_region_sizes(pm_regions);
-            let num_nodes = (region_sizes[1] - ABSOLUTE_POS_OF_LIST_REGION_NODE_START) / node_size as u64;
+            let num_nodes = (region_sizes[0] - ABSOLUTE_POS_OF_LIST_REGION_NODE_START) / node_size as u64;
             let list_node_region_header = ListRegionHeader {
                 num_nodes,
-                length: region_sizes[1],
+                length: region_sizes[0],
                 version_number: DURABLE_LIST_REGION_VERSION_NUMBER,
                 _padding0: 0,
                 program_guid: DURABLE_LIST_REGION_PROGRAM_GUID
             };
             let list_node_region_header_crc = calculate_crc(&list_node_region_header);
 
-            pm_regions.serialize_and_write(1, ABSOLUTE_POS_OF_LIST_REGION_HEADER, &list_node_region_header);
-            pm_regions.serialize_and_write(1, ABSOLUTE_POS_OF_LIST_REGION_HEADER_CRC, &list_node_region_header_crc);
+            pm_regions.serialize_and_write(0, ABSOLUTE_POS_OF_LIST_REGION_HEADER, &list_node_region_header);
+            pm_regions.serialize_and_write(0, ABSOLUTE_POS_OF_LIST_REGION_HEADER_CRC, &list_node_region_header_crc);
 
             // we don't need to do any further setup here; we only count nodes as in-use if they are
             // reachable within a list, so its fine if they contain garbage while theyre not in use
