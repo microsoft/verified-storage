1355530ad9645043c7ab7086283e7b86f962fce2
Jay Lorch <lorch@microsoft.com>
2024-07-05 14:31:44 -0700

    Remove unnecessary assume

--- storage_node/src/multilog/multilogimpl_v.rs
+++ storage_node/src/multilog/multilogimpl_v.rs
@@ -1486,7 +1486,6 @@ verus! {
                     }
                 })
         {
-            assume(false);
             // Handle error cases due to improper parameters passed to the
             // function.
 
