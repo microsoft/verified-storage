a95a100ec3b025b2236ed9a37afbeff878961a1f
hayley-leblanc <hleblanc@utexas.edu>
2024-09-20 10:21:56 -0500

    Trace workload init fn

--- evaluation/YCSB/core/src/main/java/site/ycsb/workloads/TraceWorkload.java
+++ evaluation/YCSB/core/src/main/java/site/ycsb/workloads/TraceWorkload.java
@@ -2,6 +2,7 @@ package site.ycsb.workloads;
 
 import site.ycsb.*;
 import site.ycsb.generator.*;
+import site.ycsb.generator.Uniform16ByteIDGenerator;
 import site.ycsb.measurements.Measurements;
 
 import java.util.*;
@@ -16,9 +17,19 @@ public class TraceWorkload extends Workload {
   public static final String READ_PROPORTION_PROPERTY = "readproportion";
 
   /**
+   * The default proportion of transactions that are reads.
+   */
+  public static final String READ_PROPORTION_PROPERTY_DEFAULT = "0.2";
+
+  /**
    * The name of the property for the proportion of transactions that are updates.
    */
   public static final String UPDATE_PROPORTION_PROPERTY = "updateproportion";
+
+  /**
+   * The default proportion of transactions that are updates.
+   */
+  public static final String UPDATE_PROPORTION_PROPERTY_DEFAULT = "0.05";
 
   /**
    * The name of the property for the proportion of transactions that are inserts.
@@ -26,13 +37,24 @@ public class TraceWorkload extends Workload {
   public static final String INSERT_PROPORTION_PROPERTY = "insertproportion";
 
   /**
+   * The default proportion of transactions that are inserts.
+   */
+  public static final String INSERT_PROPORTION_PROPERTY_DEFAULT = "0.75";
+
+  /**
    * The name of the property for the the distribution of requests across the keyspace. Options are
    * "uniform", "zipfian" and "latest"
    */
   public static final String REQUEST_DISTRIBUTION_PROPERTY = "requestdistribution";
 
+  /**
+   * The default distribution of requests across the keyspace.
+   */
+  public static final String REQUEST_DISTRIBUTION_PROPERTY_DEFAULT = "uniform";
+
   protected DiscreteGenerator operationchooser;
-  protected NumberGenerator keychooser;
+  protected Generator keychooser;
+  protected long recordcount;
 
   private Measurements measurements = Measurements.getMeasurements();
 
@@ -42,7 +64,67 @@ public class TraceWorkload extends Workload {
    */
   @Override
   public void init(Properties p) throws WorkloadException {
+    recordcount =
+      Long.parseLong(p.getProperty(Client.RECORD_COUNT_PROPERTY, Client.DEFAULT_RECORD_COUNT));
+    if (recordcount == 0) {
+      throw new WorkloadException("Must provide recordcount");
+    }
+    long insertstart =
+        Long.parseLong(p.getProperty(INSERT_START_PROPERTY, INSERT_START_PROPERTY_DEFAULT));
+    long insertcount =
+        Integer.parseInt(p.getProperty(INSERT_COUNT_PROPERTY, String.valueOf(recordcount - insertstart)));
+    String requestdistrib =
+        p.getProperty(REQUEST_DISTRIBUTION_PROPERTY, REQUEST_DISTRIBUTION_PROPERTY_DEFAULT);
 
+    // Confirm valid values for insertstart and insertcount in relation to recordcount
+    if (recordcount < (insertstart + insertcount)) {
+      System.err.println("Invalid combination of insertstart, insertcount and recordcount.");
+      System.err.println("recordcount must be bigger than insertstart + insertcount.");
+      System.exit(-1);
+    }
+
+    operationchooser = createOperationGenerator(p);
+    keychooser = new Uniform16ByteIDGenerator(insertstart, insertstart + insertcount - 1);
+  }
+
+  /**
+   * Taken from CoreWorkload class.
+   * We could modify the set of operations if we want to add trace-specific things.
+   * 
+   * Creates a weighted discrete values with database operations for a workload to perform.
+   * Weights/proportions are read from the properties list and defaults are used
+   * when values are not configured.
+   * Current operations are "READ", "UPDATE", "INSERT", "SCAN" and "READMODIFYWRITE".
+   *
+   * @param p The properties list to pull weights from.
+   * @return A generator that can be used to determine the next operation to perform.
+   * @throws IllegalArgumentException if the properties object was null.
+   */
+  protected static DiscreteGenerator createOperationGenerator(final Properties p) {
+    if (p == null) {
+      throw new IllegalArgumentException("Properties object cannot be null");
+    }
+    final double readproportion = Double.parseDouble(
+        p.getProperty(READ_PROPORTION_PROPERTY, READ_PROPORTION_PROPERTY_DEFAULT));
+    final double updateproportion = Double.parseDouble(
+        p.getProperty(UPDATE_PROPORTION_PROPERTY, UPDATE_PROPORTION_PROPERTY_DEFAULT));
+    final double insertproportion = Double.parseDouble(
+        p.getProperty(INSERT_PROPORTION_PROPERTY, INSERT_PROPORTION_PROPERTY_DEFAULT));
+
+    final DiscreteGenerator operationchooser = new DiscreteGenerator();
+    if (readproportion > 0) {
+      operationchooser.addValue(readproportion, "READ");
+    }
+
+    if (updateproportion > 0) {
+      operationchooser.addValue(updateproportion, "UPDATE");
+    }
+
+    if (insertproportion > 0) {
+      operationchooser.addValue(insertproportion, "INSERT");
+    }
+
+    return operationchooser;
   }
 
   /**
