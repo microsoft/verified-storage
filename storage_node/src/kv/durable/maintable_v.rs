c9500889e073834da87e9bc4ff98d3bc85f73660
Nickolai Zeldovich <nickolai@csail.mit.edu>
2024-11-19 12:53:53 -0800

    remove proof instability hack
    unused_function was necessary as of commit
    21213647ff956f84c9039931cf9d1f905cb1a3e2 but no longer needed after
    commit bcc7128554db95a99d6e3b554ca91c2809492c58

--- storage_node/src/kv/durable/maintable_v.rs
+++ storage_node/src/kv/durable/maintable_v.rs
@@ -296,13 +296,6 @@ verus! {
             subregion.is_writable_relative_addr(addr as int)
     }
 
-    // XXX
-    // The existence of this unused function allows some proofs in this file to succeed,
-    // which otherwise fail..
-    spec fn unused_function() -> () {
-        ()
-    }
-
     impl<K> MainTable<K>
         where 
             K: PmCopy + std::fmt::Debug,
