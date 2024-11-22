9bd959ef45cab69522fd612b914aa77298d85efa
Nickolai Zeldovich <nickolai@csail.mit.edu>
2024-11-22 12:01:09 -0800

    remove assume(false) from Hamming CRC proofs
    requires latest version of Verus which has some necessary lemmas about
    sequences and multisets

--- storage_node/src/pmem/hamming_v.rs
+++ storage_node/src/pmem/hamming_v.rs
@@ -70,9 +70,6 @@ verus! {
         decreases
             idx1.len()
     {
-        // XXX pending Verus PR #1329
-        assume(false);
-
         idx1.to_multiset_ensures();
         idx2.to_multiset_ensures();
         S(s)[idx1].to_multiset_ensures();
@@ -108,9 +105,7 @@ verus! {
         ensures
             sum(s1) == sum(s2)
     {
-        // XXX pending Verus PR #1329
-        assume(false);
-        // fold_right_permutation(s1, s2, |i, s: nat| { s+i as nat }, 0)
+        lemma_fold_right_permutation(s1, s2, |i, s: nat| { s+i as nat }, 0)
     }
 
     pub proof fn sum_concat(s1: Seq<nat>, s2: Seq<nat>)
@@ -120,11 +115,8 @@ verus! {
         let s = s1+s2;
         assert(s1 == s.subrange(0, s1.len() as int));
         assert(s2 == s.subrange(s1.len() as int, s.len() as int));
-
-        // XXX pending Verus PR #1329
-        assume(false);
-        // s.lemma_fold_right_split(|i, s: nat| { s+i as nat }, 0, s1.len() as int);
-        // s1.lemma_fold_right_commute_one(sum(s2), |i, s: nat| { s+i as nat }, 0);
+        s.lemma_fold_right_split(|i, s: nat| { s+i as nat }, 0, s1.len() as int);
+        s1.lemma_fold_right_commute_one(sum(s2), |i, s: nat| { s+i as nat }, 0);
     }
 
     pub proof fn sum_remove(s: Seq<nat>, i: int)
@@ -159,10 +151,7 @@ verus! {
         idx.lemma_sort_ensures();
         let idx_sorted = idx.sort();
         idx.lemma_multiset_has_no_duplicates();
-
-        // XXX pending Verus PR #1329
-        assume(false);
-        // idx_sorted.lemma_multiset_has_no_duplicates_conv();
+        idx_sorted.lemma_multiset_has_no_duplicates_conv();
 
         valid_indexes_permute(s, idx, idx_sorted);
         sum_indexes_helper(s, idx_sorted);
