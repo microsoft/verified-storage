2e5ec05a06d273ca25d7b1e0dfbc26c4740f574a
Jay Lorch <lorch@microsoft.com>
2024-12-04 15:44:30 -0800

    Remove TODO markers that have been to-done

--- storage_node/src/kv/kvimpl_t.rs
+++ storage_node/src/kv/kvimpl_t.rs
@@ -15,8 +15,6 @@
 //! different components.
 //!
 //! This file is unverified and should be tested/audited for correctness.
-//!
-//! TODO: handle errors properly in postconditions
 
 #![allow(unused_imports)]
 // #![verus::trusted]
@@ -82,8 +80,6 @@ pub closed spec fn spec_phantom_data<V: ?Sized>() -> core::marker::PhantomData<V
     core::marker::PhantomData::default()
 }
 
-// TODO: should the constructor take one PM region and break it up into the required sub-regions,
-// or should the caller provide it split up in the way that they want?
 #[verifier::reject_recursive_types(K)]
 #[verifier::reject_recursive_types(I)]
 pub struct KvStore<PM, K, I, L>
@@ -173,7 +169,7 @@ where
                 Err(KvError::InternalError) => true,
                 Err(KvError::IndexOutOfRange) => true,
                 Err(KvError::PmemErr{ pmem_err }) => true,
-                Err(_) => false // TODO
+                Err(_) => false
             }
     {
         let mut wrpm_region = WriteRestrictedPersistentMemoryRegion::new(pm_region);
@@ -236,7 +232,6 @@ where
                 },
                 Err(KvError::OutOfSpace) => {
                     &&& self@ == old(self)@.abort()
-                    // TODO
                 }
                 Err(_) => false,
             }
@@ -271,7 +266,6 @@ where
                 },
                 Err(KvError::OutOfSpace) => {
                     &&& self@ == old(self)@.abort()
-                    // TODO
                 }
                 Err(_) => false,
             }
@@ -304,7 +298,6 @@ where
                 },
                 Err(KvError::OutOfSpace) => {
                     &&& self@ == old(self)@.abort()
-                    // TODO
                 }
                 Err(_) => false,
             }
@@ -328,11 +321,9 @@ where
                 }, 
                 Err(KvError::OutOfSpace) => {
                     &&& self@ == old(self)@.abort()
-                    // TODO
                 }
                 Err(KvError::LogErr { log_err }) => {
                     &&& self@ == old(self)@.abort()
-                    // TODO
                 }
                 Err(KvError::NoCurrentTransaction) => {
                     &&& self@ == old(self)@
@@ -347,386 +338,6 @@ where
         let tracked perm = TrustedKvPermission::new_two_possibilities(self.id, self@.durable, self@.tentative);
         self.untrusted_kv_impl.untrusted_commit(self.id, Tracked(&perm))
     }
-
-    /* 
-    pub fn start(
-        metadata_pmem: PM,
-        item_table_pmem: PM,
-        list_pmem: PM,
-        log_pmem: PM,
-        kvstore_id: u128,
-        num_keys: u64,
-        node_size: u32,
-    ) -> (result: Result<Self, KvError<K>>)
-        requires 
-            // TODO 
-        ensures 
-            // TODO 
-    {
-        assume(false);
-        let kv = UntrustedKvStoreImpl::<PM, K, I, L, V>::untrusted_start(metadata_pmem, item_table_pmem, list_pmem, log_pmem, kvstore_id, num_keys, node_size)?;
-        Ok(Self {
-            untrusted_kv_impl: kv,
-            id: kvstore_id,
-        })
-    }
-
-//     // fn restore(pmem: PM, region_size: usize, kvstore_id: u128) -> (result: Result<Self, KvError<K>>)
-//     //     requires
-//     //         pmem.inv(),
-//     //     ensures
-//     //         match result {
-//     //             Ok(restored_kv) => {
-//     //                 let restored_state = UntrustedKvStoreImpl::<PM, K, I, L, V, E>::recover(pmem@.durable_state, kvstore_id);
-//     //                 match restored_state {
-//     //                     Some(restored_state) => restored_kv@ == restored_state,
-//     //                     None => false
-//     //                 }
-//     //             }
-//     //             Err(_) => true // TODO
-//     //         }
-//     // {
-//     //     Err(KvError::NotImplemented)
-//     // }
-
-    pub fn create(&mut self, key: &K, item: &I, kvstore_id: u128) -> (result: Result<(), KvError<K>>)
-        requires
-            // old(self).valid(),
-        ensures
-            // self.valid(),
-            // match result {
-            //     Ok(()) => {
-            //         &&& self@ == old(self)@.create(*key, *item).unwrap()
-            //     }
-            //     Err(KvError::KeyAlreadyExists) => {
-            //         &&& old(self)@.contents.contains_key(*key)
-            //         &&& old(self)@ == self@
-            //     }
-            //     Err(_) => false
-            // }
-    {
-        assume(false);
-        if self.untrusted_kv_impl.untrusted_contains_key(key) {
-            Err(KvError::KeyAlreadyExists)
-        } else {
-            let tracked perm =
-                TrustedKvPermission::new_two_possibilities(self.id, self@, self@.create(*key, *item).unwrap());
-            self.untrusted_kv_impl.untrusted_create(key, item, kvstore_id, Tracked(&perm))
-        }
-    }
-
-    pub fn read_item(&self, key: &K) -> (result: Result<Box<I>, KvError<K>>)
-        requires
-            // self.valid()
-        ensures
-        // ({
-        //     let spec_result = self@.read_item_and_list(*key);
-        //     match (result, spec_result) {
-        //         (Some(output_item), Some((spec_item, pages))) => {
-        //             &&& spec_item == output_item
-        //         }
-        //         (Some(output_item), None) => false,
-        //         (None, Some((spec_item, pages))) => false,
-        //         (None, None) => true,
-        //     }
-        // })
-    {
-        assume(false);
-        self.untrusted_kv_impl.untrusted_read_item(key)
-    }
-
-//     // fn read_item_and_list(&self, key: &K) -> (result: Option<(&I, Vec<&L>)>)
-//     //     requires
-//     //         self.valid(),
-//     //     ensures
-//     //     ({
-//     //         let spec_result = self@.read_item_and_list(*key);
-//     //         match (result, spec_result) {
-//     //             (Some((output_item, output_pages)), Some((spec_item, spec_pages))) => {
-//     //                 &&& spec_item == output_item
-//     //                 &&& spec_pages == output_pages@
-//     //             }
-//     //             (Some((output_item, output_pages)), None) => false,
-//     //             (None, Some((spec_item, spec_pages))) => false,
-//     //             (None, None) => true,
-//     //         }
-//     //     })
-//     // {
-//     //     self.untrusted_kv_impl.untrusted_read_item_and_list(key)
-//     // }
-
-//     fn read_list_entry_at_index(&self, key: &K, idx: u64) -> (result: Result<&L, KvError<K>>)
-//         requires
-//             self.valid()
-//         ensures
-//             ({
-//                 let spec_result = self@.read_list_entry_at_index(*key, idx as int);
-//                 match (result, spec_result) {
-//                     (Ok(output_entry), Ok(spec_entry)) => {
-//                         &&& output_entry == spec_entry
-//                     }
-//                     (Err(KvError::IndexOutOfRange), Err(KvError::IndexOutOfRange)) => {
-//                         &&& self@.contents.contains_key(*key)
-//                         &&& self@.contents[*key].1.len() <= idx
-//                     }
-//                     (Err(KvError::KeyNotFound), Err(KvError::KeyNotFound)) => {
-//                         &&& !self@.contents.contains_key(*key)
-//                     }
-//                     (_, _) => false
-//                 }
-//             })
-//     {
-//         self.untrusted_kv_impl.untrusted_read_list_entry_at_index(key, idx)
-//     }
-
-//     // fn read_list(&self, key: &K) -> (result: Option<&Vec<L>>)
-//     //     requires
-//     //         self.valid(),
-//     //     ensures
-//     //     ({
-//     //         let spec_result = self@.read_item_and_list(*key);
-//     //         match (result, spec_result) {
-//     //             (Some(output_pages), Some((spec_item, spec_pages))) => {
-//     //                 &&& spec_pages == output_pages@
-//     //             }
-//     //             (Some(output_pages), None) => false,
-//     //             (None, Some((spec_item, spec_pages))) => false,
-//     //             (None, None) => true,
-//     //         }
-//     //     })
-//     // {
-//     //     self.untrusted_kv_impl.untrusted_read_list(key)
-//     // }
-
-    pub fn update_item(&mut self, key: &K, new_item: &I, kvstore_id: u128) -> (result: Result<(), KvError<K>>)
-        requires
-            old(self).valid(),
-        ensures
-            self.valid(),
-            match result {
-                Ok(()) => {
-                    &&& self@ == old(self)@.update_item(*key, *new_item).unwrap()
-                }
-                Err(KvError::KeyNotFound) => {
-                    &&& !old(self)@.contents.contains_key(*key)
-                    &&& old(self)@ == self@
-                }
-                Err(_) => false
-            }
-    {
-        if self.untrusted_kv_impl.untrusted_contains_key(key) {
-            let tracked perm = TrustedKvPermission::new_two_possibilities(self.id, self@, self@.update_item(*key, *new_item).unwrap());
-            self.untrusted_kv_impl.untrusted_update_item(key, new_item, kvstore_id, Tracked(&perm))
-        } else {
-            Err(KvError::KeyNotFound)
-        }
-
-    }
-    */
-
-//     fn delete(&mut self, key: &K, kvstore_id: u128,) -> (result: Result<(), KvError<K>>)
-//         requires
-//             old(self).valid()
-//         ensures
-//             self.valid(),
-//             match result {
-//                 Ok(()) => {
-//                     &&& self@ == old(self)@.delete(*key).unwrap()
-//                 }
-//                 Err(KvError::KeyNotFound) => {
-//                     &&& !old(self)@.contents.contains_key(*key)
-//                     &&& old(self)@ == self@
-//                 }
-//                 Err(_) => false
-//             }
-//     {
-//         if self.untrusted_kv_impl.untrusted_contains_key(key) {
-//             let tracked perm = TrustedKvPermission::new_two_possibilities(self.id, self@, self@.delete(*key).unwrap());
-//             self.untrusted_kv_impl.untrusted_delete(key, kvstore_id, Tracked(&perm))
-//         } else {
-//             Err(KvError::KeyNotFound)
-//         }
-//     }
-
-//     // TODO: remove?
-//     fn append_to_list(
-//         &mut self,
-//         key: &K,
-//         new_list_entry: L
-//     ) -> (result: Result<(), KvError<K>>)
-//         requires
-//             old(self).valid()
-//         ensures
-//             self.valid(),
-//             match result {
-//                 Ok(()) => {
-//                     &&& self@ == old(self)@.append_to_list(*key, new_list_entry).unwrap()
-//                 }
-//                 Err(KvError::KeyNotFound) => {
-//                     &&& !old(self)@.contents.contains_key(*key)
-//                     &&& old(self)@ == self@
-//                 }
-//                 // TODO: case for if we run out of space to append to the list
-//                 Err(_) => false
-//             }
-//     {
-//         if self.untrusted_kv_impl.untrusted_contains_key(key) {
-//             let tracked perm = TrustedKvPermission::new_two_possibilities(self.id, self@, self@.append_to_list(*key, new_list_entry).unwrap());
-//             self.untrusted_kv_impl.untrusted_append_to_list(key, new_list_entry, Tracked(&perm))
-//         } else {
-//             Err(KvError::KeyNotFound)
-//         }
-//     }
-
-//     fn append_to_list_and_update_item(
-//         &mut self,
-//         key: &K,
-//         new_list_entry: L,
-//         new_item: I,
-//     ) -> (result: Result<(), KvError<K>>)
-//         requires
-//             old(self).valid()
-//         ensures
-//             self.valid(),
-//             match result {
-//                 Ok(()) => {
-//                     &&& self@ == old(self)@.append_to_list_and_update_item(*key, new_list_entry, new_item).unwrap()
-//                 }
-//                 Err(KvError::KeyNotFound) => {
-//                     &&& !old(self)@.contents.contains_key(*key)
-//                     &&& old(self)@ == self@
-//                 }
-//                 // TODO: case for if we run out of space to append to the list
-//                 Err(_) => false
-//             }
-//     {
-//         if self.untrusted_kv_impl.untrusted_contains_key(key) {
-//             let tracked perm = TrustedKvPermission::new_two_possibilities(self.id, self@, self@.append_to_list_and_update_item(*key, new_list_entry, new_item).unwrap());
-//             self.untrusted_kv_impl.untrusted_append_to_list_and_update_item(key,  new_list_entry, new_item, Tracked(&perm))
-//         } else {
-//             Err(KvError::KeyNotFound)
-//         }
-//     }
-
-//     fn update_list_entry_at_index(&mut self, key: &K, idx: usize, new_list_entry: L) -> (result: Result<(), KvError<K>>)
-//         requires
-//             old(self).valid()
-//         ensures
-//             self.valid(),
-//             match result {
-//                 Ok(()) => {
-//                     &&& self@ == old(self)@.update_list_entry_at_index(*key, idx, new_list_entry).unwrap()
-//                 }
-//                 Err(KvError::KeyNotFound) => {
-//                     &&& !old(self)@.contents.contains_key(*key)
-//                     &&& old(self)@ == self@
-//                 }
-//                 Err(_) => false
-//             }
-//     {
-//         if self.untrusted_kv_impl.untrusted_contains_key(key) {
-//             let tracked perm = TrustedKvPermission::new_two_possibilities(self.id, self@, self@.update_list_entry_at_index(*key, idx, new_list_entry).unwrap());
-//             self.untrusted_kv_impl.untrusted_update_list_entry_at_index(key, idx, new_list_entry, Tracked(&perm))
-//         } else {
-//             Err(KvError::KeyNotFound)
-//         }
-//     }
-
-//     fn update_entry_at_index_and_item(
-//         &mut self,
-//         key: &K,
-//         idx: usize,
-//         new_list_entry: L,
-//         new_item: I,
-//     ) -> (result: Result<(), KvError<K>>)
-//         requires
-//             old(self).valid()
-//         ensures
-//             match result {
-//                 Ok(()) => {
-//                     &&& self.valid()
-//                     &&& self@ == old(self)@.update_entry_at_index_and_item(*key, idx, new_list_entry, new_item).unwrap()
-//                 }
-//                 Err(KvError::KeyNotFound) => {
-//                     &&& !old(self)@.contents.contains_key(*key)
-//                     &&& old(self)@ == self@
-//                 }
-//                 Err(_) => false
-//             }
-//     {
-//         if self.untrusted_kv_impl.untrusted_contains_key(key) {
-//             let tracked perm = TrustedKvPermission::new_two_possibilities(self.id, self@, self@.update_entry_at_index_and_item(*key, idx, new_list_entry, new_item).unwrap());
-//             self.untrusted_kv_impl.untrusted_update_entry_at_index_and_item(key,  idx, new_list_entry, new_item, Tracked(&perm))
-//         } else {
-//             Err(KvError::KeyNotFound)
-//         }
-//     }
-
-//     fn trim_list(
-//         &mut self,
-//         key: &K,
-//         trim_length: usize,
-//     ) -> (result: Result<(), KvError<K>>)
-//         requires
-//             old(self).valid()
-//         ensures
-//             match result {
-//                 Ok(()) => {
-//                     &&& self.valid()
-//                     &&& self@ == old(self)@.trim_list(*key, trim_length as int).unwrap()
-//                 }
-//                 Err(KvError::KeyNotFound) => {
-//                     &&& !old(self)@.contents.contains_key(*key)
-//                     &&& old(self)@ == self@
-//                 }
-//                 Err(_) => false
-//             }
-//     {
-//         if self.untrusted_kv_impl.untrusted_contains_key(key) {
-//             let tracked perm = TrustedKvPermission::new_two_possibilities(self.id, self@, self@.trim_list(*key, trim_length as int).unwrap());
-//             self.untrusted_kv_impl.untrusted_trim_list(key, trim_length, Tracked(&perm))
-//         } else {
-//             Err(KvError::KeyNotFound)
-//         }
-//     }
-
-//     fn trim_list_and_update_item(
-//         &mut self,
-//         key: &K,
-//         trim_length: usize,
-//         new_item: I,
-//     ) -> (result: Result<(), KvError<K>>)
-//         requires
-//             old(self).valid()
-//         ensures
-//             match result {
-//                 Ok(()) => {
-//                     &&& self.valid()
-//                     &&& self@ == old(self)@.trim_list_and_update_item(*key, trim_length as int, new_item).unwrap()
-//                 }
-//                 Err(KvError::KeyNotFound) => {
-//                     &&& !old(self)@.contents.contains_key(*key)
-//                     &&& old(self)@ == self@
-//                 }
-//                 Err(_) => false
-//             }
-//     {
-//         if self.untrusted_kv_impl.untrusted_contains_key(key) {
-//             let tracked perm = TrustedKvPermission::new_two_possibilities(self.id, self@, self@.trim_list_and_update_item(*key, trim_length as int, new_item).unwrap());
-//             self.untrusted_kv_impl.untrusted_trim_list_and_update_item(key, trim_length, new_item, Tracked(&perm))
-//         } else {
-//             Err(KvError::KeyNotFound)
-//         }
-//     }
-
-//     fn get_keys(&self) -> (result: Vec<K>)
-//         requires
-//             self.valid()
-//         ensures
-//             result@.to_set() == self@.get_keys()
-//     {
-//         self.untrusted_kv_impl.untrusted_get_keys()
-//     }
 }
 
 }
