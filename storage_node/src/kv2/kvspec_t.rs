8accec9c6a008cdfa382888eacc7f8413f46b0fa
Jay Lorch <lorch@microsoft.com>
2025-01-02 15:13:21 -0800

    Make validity part of constants_match

--- storage_node/src/kv2/kvspec_t.rs
+++ storage_node/src/kv2/kvspec_t.rs
@@ -412,6 +412,8 @@ where
 
     pub open spec fn constants_match(self, other: Self) -> bool
     {
+        &&& self.valid()
+        &&& other.valid()
         &&& self.id == other.id
         &&& self.logical_range_gaps_policy == other.logical_range_gaps_policy
         &&& self.pm_constants == other.pm_constants
