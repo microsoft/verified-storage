f222d87efb1e03d09df71d0683b27d03888e6b60
hayley-leblanc <hleblanc@utexas.edu>
2025-01-22 12:48:00 -0600

    More block list trim tests

--- list_experiment/src/main.rs
+++ list_experiment/src/main.rs
@@ -118,7 +118,6 @@ mod tests {
             i += 1;
         }
 
-        let vec_list = list.read_full_list(&mock_pool, &list_table).unwrap();
         list.trim(&mut mock_pool, &mut list_table, trim_len)
             .unwrap();
 
@@ -127,5 +126,99 @@ mod tests {
         for i in 0..vec_list.len() {
             assert!(u64::from_le_bytes(vec_list[i]) == i as u64 + trim_len);
         }
+
+        // we can append new elements successfully
+        let val: u64 = 1;
+        let val_bytes = val.to_le_bytes();
+        list.append(&mut mock_pool, &mut list_table, val_bytes)
+            .unwrap();
+        let vec_list = list.read_full_list(&mock_pool, &list_table).unwrap();
+        assert!(vec_list.len() == (list_len - trim_len) as usize + 1);
+        for i in 0..vec_list.len() - 1 {
+            assert!(u64::from_le_bytes(vec_list[i]) == i as u64 + trim_len);
+        }
+        assert!(u64::from_le_bytes(vec_list[vec_list.len() - 1]) == 1);
+    }
+
+    #[test]
+    fn trim_block_list_on_mock2() {
+        const ELEMENT_SIZE: usize = 8;
+        const ROWS_PER_BLOCK: usize = 4;
+        let list_len = 7;
+        let trim_len = 4;
+
+        let mut mock_pool = MockPool::new(1024 * 1024);
+        let mut list_table: BlockListTable<ELEMENT_SIZE, ROWS_PER_BLOCK> =
+            BlockListTable::new(0, mock_pool.len());
+        let mut list: DurableBlockList<ELEMENT_SIZE, ROWS_PER_BLOCK> = DurableBlockList::new();
+
+        let mut i: u64 = 0;
+        while i < list_len {
+            let val_bytes = i.to_le_bytes();
+            list.append(&mut mock_pool, &mut list_table, val_bytes)
+                .unwrap();
+            i += 1;
+        }
+
+        list.trim(&mut mock_pool, &mut list_table, trim_len)
+            .unwrap();
+
+        let vec_list = list.read_full_list(&mock_pool, &list_table).unwrap();
+        assert!(vec_list.len() == (list_len - trim_len) as usize);
+        for i in 0..vec_list.len() {
+            assert!(u64::from_le_bytes(vec_list[i]) == i as u64 + trim_len);
+        }
+
+        // we can append new elements successfully
+        let val: u64 = 1;
+        let val_bytes = val.to_le_bytes();
+        list.append(&mut mock_pool, &mut list_table, val_bytes)
+            .unwrap();
+        let vec_list = list.read_full_list(&mock_pool, &list_table).unwrap();
+        assert!(vec_list.len() == (list_len - trim_len) as usize + 1);
+        for i in 0..vec_list.len() - 1 {
+            assert!(u64::from_le_bytes(vec_list[i]) == i as u64 + trim_len);
+        }
+        assert!(u64::from_le_bytes(vec_list[vec_list.len() - 1]) == 1);
+    }
+
+    #[test]
+    fn trim_block_list_on_mock3() {
+        const ELEMENT_SIZE: usize = 8;
+        const ROWS_PER_BLOCK: usize = 4;
+        let list_len = 7;
+        let trim_len = 7;
+
+        let mut mock_pool = MockPool::new(1024 * 1024);
+        let mut list_table: BlockListTable<ELEMENT_SIZE, ROWS_PER_BLOCK> =
+            BlockListTable::new(0, mock_pool.len());
+        let mut list: DurableBlockList<ELEMENT_SIZE, ROWS_PER_BLOCK> = DurableBlockList::new();
+
+        let mut i: u64 = 0;
+        while i < list_len {
+            let val_bytes = i.to_le_bytes();
+            list.append(&mut mock_pool, &mut list_table, val_bytes)
+                .unwrap();
+            i += 1;
+        }
+
+        list.trim(&mut mock_pool, &mut list_table, trim_len)
+            .unwrap();
+
+        let vec_list = list.read_full_list(&mock_pool, &list_table).unwrap();
+        assert!(vec_list.len() == (list_len - trim_len) as usize);
+        for i in 0..vec_list.len() {
+            assert!(u64::from_le_bytes(vec_list[i]) == i as u64 + trim_len);
+        }
+        assert!(vec_list.len() == 0);
+
+        // we can append new elements successfully
+        let val: u64 = 1;
+        let val_bytes = val.to_le_bytes();
+        list.append(&mut mock_pool, &mut list_table, val_bytes)
+            .unwrap();
+        let vec_list = list.read_full_list(&mock_pool, &list_table).unwrap();
+        assert!(vec_list.len() == 1);
+        assert!(u64::from_le_bytes(vec_list[0]) == 1);
     }
 }
