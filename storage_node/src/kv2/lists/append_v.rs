678fd5a5fe3a9165447eac6f1e78874fb9e26f04
Jay Lorch <lorch@microsoft.com>
2025-01-23 15:55:00 -0800

    Add missing file

---
+++ storage_node/src/kv2/lists/append_v.rs
@@ -0,0 +1,150 @@
+#![allow(unused_imports)]
+use builtin::*;
+use builtin_macros::*;
+use vstd::prelude::*;
+
+use crate::common::align_v::*;
+use crate::common::overflow_v::*;
+use crate::common::recover_v::*;
+use crate::common::subrange_v::*;
+use crate::common::table_v::*;
+use crate::common::util_v::*;
+use crate::journal::*;
+use crate::pmem::pmemspec_t::*;
+use crate::pmem::pmcopy_t::*;
+use crate::pmem::traits_t::*;
+use crate::pmem::wrpm_t::*;
+use crate::pmem::pmemutil_v::*;
+use deps_hack::PmCopy;
+use std::collections::HashMap;
+use std::hash::Hash;
+use super::*;
+use super::recover_v::*;
+use super::super::impl_t::*;
+use super::super::spec_t::*;
+
+verus! {
+
+impl<PM, L> ListTable<PM, L>
+    where
+        PM: PersistentMemoryRegion,
+        L: PmCopy + LogicalRange + Sized + std::fmt::Debug,
+{
+    pub exec fn append(
+        &mut self,
+        row_addr: u64,
+        new_list_entry: L,
+        journal: &mut Journal<TrustedKvPermission, PM>,
+        Tracked(perm): Tracked<&TrustedKvPermission>,
+    ) -> (result: Result<u64, KvError>)
+        requires
+            old(self).valid(old(journal)@),
+            old(self)@.tentative is Some,
+            old(self)@.tentative.unwrap().m.contains_key(row_addr),
+        ensures
+            self.valid(journal@),
+            journal.valid(),
+            journal@.constants_match(old(journal)@),
+            old(journal)@.matches_except_in_range(journal@, self@.sm.start() as int, self@.sm.end() as int),
+            match result {
+                Ok(new_row_addr) => {
+                    &&& new_row_addr != 0
+                    &&& new_row_addr == row_addr || !old(self)@.tentative.unwrap().m.contains_key(new_row_addr)
+                    &&& match self@.logical_range_gaps_policy {
+                        LogicalRangeGapsPolicy::LogicalRangeGapsPermitted =>
+                            new_list_entry.start() >= end_of_range(old(self)@.tentative.unwrap().m[row_addr]),
+                        LogicalRangeGapsPolicy::LogicalRangeGapsForbidden =>
+                            new_list_entry.start() == end_of_range(old(self)@.tentative.unwrap().m[row_addr]),
+                    }
+                    &&& self@ == (ListTableView {
+                        tentative: Some(old(self)@.tentative.unwrap().append(row_addr, new_row_addr, new_list_entry)),
+                        ..old(self)@
+                    })
+                    &&& self.validate_list_addr(new_row_addr)
+                },
+                Err(KvError::PageLeavesLogicalRangeGap{ end_of_valid_range }) => {
+                    &&& self@ == old(self)@
+                    &&& self@.logical_range_gaps_policy is LogicalRangeGapsForbidden
+                    &&& new_list_entry.start() > end_of_range(old(self)@.tentative.unwrap().m[row_addr])
+                    &&& end_of_valid_range == end_of_range(old(self)@.tentative.unwrap().m[row_addr])
+                },
+                Err(KvError::PageOutOfLogicalRangeOrder{ end_of_valid_range }) => {
+                    &&& self@ == old(self)@
+                    &&& new_list_entry.start() < end_of_range(old(self)@.tentative.unwrap().m[row_addr])
+                    &&& end_of_valid_range == end_of_range(old(self)@.tentative.unwrap().m[row_addr])
+                }
+                Err(KvError::OutOfSpace) => {
+                    &&& self@ == (ListTableView {
+                        tentative: None,
+                        ..old(self)@
+                    })
+                },
+                Err(KvError::CRCMismatch) => {
+                    &&& !journal@.pm_constants.impervious_to_corruption()
+                    &&& self@ == (ListTableView {
+                        tentative: None,
+                        ..old(self)@
+                    })
+                },
+                _ => false,
+            }
+    {
+        assume(false);
+        Err(KvError::NotImplemented)
+    }
+
+    pub exec fn create_singleton(
+        &mut self,
+        new_list_entry: L,
+        journal: &mut Journal<TrustedKvPermission, PM>,
+        Tracked(perm): Tracked<&TrustedKvPermission>,
+    ) -> (result: Result<u64, KvError>)
+        requires
+            old(self).valid(old(journal)@),
+            old(self)@.tentative is Some,
+        ensures
+            self.valid(journal@),
+            journal.valid(),
+            journal@.constants_match(old(journal)@),
+            old(journal)@.matches_except_in_range(journal@, self@.sm.start() as int, self@.sm.end() as int),
+            match result {
+                Ok(new_row_addr) => {
+                    &&& new_row_addr != 0
+                    &&& self@.logical_range_gaps_policy is LogicalRangeGapsForbidden ==>
+                        new_list_entry.start() == 0
+                    &&& !old(self)@.tentative.unwrap().m.contains_key(new_row_addr)
+                    &&& self@ == (ListTableView {
+                        tentative: Some(old(self)@.tentative.unwrap().create_singleton(new_row_addr, new_list_entry)),
+                        ..old(self)@
+                    })
+                    &&& self.validate_list_addr(new_row_addr)
+                },
+                Err(KvError::PageLeavesLogicalRangeGap{ end_of_valid_range }) => {
+                    &&& self@ == old(self)@
+                    &&& self@.logical_range_gaps_policy is LogicalRangeGapsForbidden
+                    &&& new_list_entry.start() != 0
+                    &&& end_of_valid_range == 0
+                }
+                Err(KvError::OutOfSpace) => {
+                    &&& self@ == (ListTableView {
+                        tentative: None,
+                        ..old(self)@
+                    })
+                },
+                Err(KvError::CRCMismatch) => {
+                    &&& !journal@.pm_constants.impervious_to_corruption()
+                    &&& self@ == (ListTableView {
+                        tentative: None,
+                        ..old(self)@
+                    })
+                },
+                _ => false,
+            }
+    {
+        assume(false);
+        Err(KvError::NotImplemented)
+    }
+}
+
+}
+
