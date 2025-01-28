45e6f9ead7c421694568180d654baf1b637e8af2
Jay Lorch <lorch@microsoft.com>
2025-01-27 18:15:40 -0800

    Improve verification time by factoring out tentatively_append_to_list_step1

--- storage_node/src/kv2/elements_v.rs
+++ storage_node/src/kv2/elements_v.rs
@@ -147,6 +147,108 @@ where
         self.lists.read_entry_at_index(list_addr, idx, &self.journal)
     }
 
+    #[inline]
+    exec fn tentatively_append_to_list_step1(
+        &mut self,
+        key: &K,
+        key_addr: u64,
+        former_rm: &KeyTableRowMetadata,
+        new_list_entry: L,
+        Tracked(perm): Tracked<&TrustedKvPermission>
+    ) -> (result: Result<u64, KvError>)
+        requires
+            old(self).valid(),
+            forall |s| #[trigger] perm.check_permission(s) <==> Self::recover(s) == Some(old(self)@.durable),
+            old(self).keys@.tentative is Some,
+            old(self).keys@.tentative.unwrap().key_info.contains_key(*key),
+            old(self).keys@.tentative.unwrap().key_info[*key] == *former_rm,
+        ensures
+            match result {
+                Ok(list_addr) => {
+                    let old_list = if former_rm.list_addr == 0 { Seq::<L>::empty() }
+                                   else { old(self).lists@.tentative.unwrap().m[former_rm.list_addr] };
+                    &&& self == Self{ status: Ghost(KvStoreStatus::ComponentsDontCorrespond),
+                                     journal: self.journal, lists: self.lists, ..*old(self) }
+                    &&& self.inv()
+                    &&& list_addr != 0
+                    &&& list_addr == former_rm.list_addr || !old(self).lists@.tentative.unwrap().m.contains_key(list_addr)
+                    &&& self.lists.validate_list_addr(list_addr)
+                    &&& self.lists@ == ListTableView { tentative: self.lists@.tentative, ..old(self).lists@ }
+                    &&& self.lists@.tentative is Some
+                    &&& self.lists@.tentative.unwrap() == if former_rm.list_addr == 0 {
+                        old(self).lists@.tentative.unwrap().create_singleton(list_addr, new_list_entry)
+                    } else {
+                        old(self).lists@.tentative.unwrap().append(former_rm.list_addr, list_addr, new_list_entry)
+                    }
+                    &&& match self.lists@.logical_range_gaps_policy {
+                        LogicalRangeGapsPolicy::LogicalRangeGapsForbidden =>
+                            new_list_entry.start() == end_of_range(old_list),
+                        LogicalRangeGapsPolicy::LogicalRangeGapsPermitted =>
+                            new_list_entry.start() >= end_of_range(old_list),
+                    }
+                    &&& self.journal@.matches_except_in_range(old(self).journal@, self.lists@.sm.start() as int,
+                                                            self.lists@.sm.end() as int)
+                },
+                Err(KvError::CRCMismatch) => {
+                    &&& self.valid()
+                    &&& self@ == old(self)@.abort()
+                    &&& !self@.pm_constants.impervious_to_corruption()
+                }, 
+                Err(KvError::OutOfSpace) => {
+                    &&& self.valid()
+                    &&& self@ == old(self)@.abort()
+                },
+                Err(e) => {
+                    &&& self.valid()
+                    &&& self@ == old(self)@
+                    &&& old(self)@.tentative.append_to_list(*key, new_list_entry) matches Err(e_spec)
+                    &&& e == e_spec
+                },
+            },
+    {
+        proof {
+            self.keys.lemma_valid_implications(self.journal@);
+        }
+
+        self.status = Ghost(KvStoreStatus::ComponentsDontCorrespond);
+
+        let ghost self_before_list_append = self.lemma_prepare_for_list_table_update(perm);
+        let result =
+            if former_rm.list_addr == 0 {
+                assert(end_of_range(Seq::<L>::empty()) == 0);
+                self.lists.create_singleton(new_list_entry, &mut self.journal, Tracked(perm))
+            }
+            else {
+                self.lists.append(former_rm.list_addr, new_list_entry, &mut self.journal, Tracked(perm))
+            };
+        proof { self.lemma_reflect_list_table_update(self_before_list_append); }
+
+        let list_addr = match result {
+            Ok(i) => i,
+            Err(KvError::CRCMismatch) => {
+                self.status = Ghost(KvStoreStatus::MustAbort);
+                self.internal_abort(Tracked(perm));
+                return Err(KvError::CRCMismatch);
+            },
+            Err(KvError::OutOfSpace) => {
+                self.status = Ghost(KvStoreStatus::MustAbort);
+                self.internal_abort(Tracked(perm));
+                return Err(KvError::OutOfSpace);
+            },
+            Err(KvError::PageLeavesLogicalRangeGap{ end_of_valid_range }) => {
+                self.status = Ghost(KvStoreStatus::Quiescent);
+                return Err(KvError::PageLeavesLogicalRangeGap{ end_of_valid_range });
+            },
+            Err(KvError::PageOutOfLogicalRangeOrder{ end_of_valid_range }) => {
+                self.status = Ghost(KvStoreStatus::Quiescent);
+                return Err(KvError::PageOutOfLogicalRangeOrder{ end_of_valid_range });
+            },
+            _ => { assert(false); return Err(KvError::InternalError); },
+        };
+
+        Ok(list_addr)
+    }
+
     pub exec fn tentatively_append_to_list(
         &mut self,
         key: &K,
@@ -187,40 +289,10 @@ where
             None => { return Err(KvError::KeyNotFound); },
         };
 
-        self.status = Ghost(KvStoreStatus::ComponentsDontCorrespond);
-
-        let ghost self_before_list_append = self.lemma_prepare_for_list_table_update(perm);
-        let result =
-            if former_rm.list_addr == 0 {
-                assert(end_of_range(Seq::<L>::empty()) == 0);
-                self.lists.create_singleton(new_list_entry, &mut self.journal, Tracked(perm))
-            }
-            else {
-                self.lists.append(former_rm.list_addr, new_list_entry, &mut self.journal, Tracked(perm))
-            };
-        proof { self.lemma_reflect_list_table_update(self_before_list_append); }
-
-        let list_addr = match result {
-            Ok(i) => i,
-            Err(KvError::CRCMismatch) => {
-                self.status = Ghost(KvStoreStatus::MustAbort);
-                self.internal_abort(Tracked(perm));
-                return Err(KvError::CRCMismatch);
-            },
-            Err(KvError::OutOfSpace) => {
-                self.status = Ghost(KvStoreStatus::MustAbort);
-                self.internal_abort(Tracked(perm));
-                return Err(KvError::OutOfSpace);
-            },
-            Err(KvError::PageLeavesLogicalRangeGap{ end_of_valid_range }) => {
-                self.status = Ghost(KvStoreStatus::Quiescent);
-                return Err(KvError::PageLeavesLogicalRangeGap{ end_of_valid_range });
-            },
-            Err(KvError::PageOutOfLogicalRangeOrder{ end_of_valid_range }) => {
-                self.status = Ghost(KvStoreStatus::Quiescent);
-                return Err(KvError::PageOutOfLogicalRangeOrder{ end_of_valid_range });
-            },
-            _ => { assert(false); return Err(KvError::InternalError); },
+        let list_addr = match self.tentatively_append_to_list_step1(key, key_addr, &former_rm, new_list_entry,
+                                                                    Tracked(perm)) {
+            Ok(a) => a,
+            Err(e) => { return Err(e); },
         };
 
         if list_addr != former_rm.list_addr {
@@ -289,40 +361,10 @@ where
             None => { return Err(KvError::KeyNotFound); },
         };
 
-        self.status = Ghost(KvStoreStatus::ComponentsDontCorrespond);
-
-        let ghost self_before_list_append = self.lemma_prepare_for_list_table_update(perm);
-        let result =
-            if former_rm.list_addr == 0 {
-                assert(end_of_range(Seq::<L>::empty()) == 0);
-                self.lists.create_singleton(new_list_entry, &mut self.journal, Tracked(perm))
-            }
-            else {
-                self.lists.append(former_rm.list_addr, new_list_entry, &mut self.journal, Tracked(perm))
-            };
-        proof { self.lemma_reflect_list_table_update(self_before_list_append); }
-
-        let list_addr = match result {
-            Ok(i) => i,
-            Err(KvError::CRCMismatch) => {
-                self.status = Ghost(KvStoreStatus::MustAbort);
-                self.internal_abort(Tracked(perm));
-                return Err(KvError::CRCMismatch);
-            },
-            Err(KvError::OutOfSpace) => {
-                self.status = Ghost(KvStoreStatus::MustAbort);
-                self.internal_abort(Tracked(perm));
-                return Err(KvError::OutOfSpace);
-            },
-            Err(KvError::PageLeavesLogicalRangeGap{ end_of_valid_range }) => {
-                self.status = Ghost(KvStoreStatus::Quiescent);
-                return Err(KvError::PageLeavesLogicalRangeGap{ end_of_valid_range });
-            },
-            Err(KvError::PageOutOfLogicalRangeOrder{ end_of_valid_range }) => {
-                self.status = Ghost(KvStoreStatus::Quiescent);
-                return Err(KvError::PageOutOfLogicalRangeOrder{ end_of_valid_range });
-            },
-            _ => { assert(false); return Err(KvError::InternalError); },
+        let list_addr = match self.tentatively_append_to_list_step1(key, key_addr, &former_rm, new_list_entry,
+                                                                    Tracked(perm)) {
+            Ok(a) => a,
+            Err(e) => { return Err(e); },
         };
 
         let ghost self_before_item_create = self.lemma_prepare_for_item_table_update(perm);
