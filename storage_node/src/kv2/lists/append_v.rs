ff339f33417c75fca88e1fd52eefecfb991dba7a
Jay Lorch <lorch@microsoft.com>
2025-02-05 13:36:13 -0800

    More efficient treatment of writing to journal

--- storage_node/src/kv2/lists/append_v.rs
+++ storage_node/src/kv2/lists/append_v.rs
@@ -155,10 +155,8 @@ impl<PM, L> ListTable<PM, L>
 
     proof fn lemma_writing_to_free_slot_has_permission_later_forall(
         iv: ListTableInternalView<L>,
-        initial_durable_state: Seq<u8>,
-        initial_read_state: Seq<u8>,
+        initial_jv: JournalView,
         sm: ListTableStaticMetadata,
-        constants: JournalConstants,
         free_list_pos: int,
         row_addr: u64,
         tracked perm: &TrustedKvPermission,
@@ -166,61 +164,94 @@ impl<PM, L> ListTable<PM, L>
         requires
             sm.valid::<L>(),
             iv.valid(sm),
-            iv.corresponds_to_durable_state(initial_durable_state, sm),
-            iv.corresponds_to_durable_state(initial_read_state, sm),
-            Journal::<TrustedKvPermission, PM>::state_recovery_idempotent(initial_durable_state, constants),
+            iv.corresponds_to_durable_state(initial_jv.durable_state, sm),
+            iv.corresponds_to_durable_state(initial_jv.read_state, sm),
+            Journal::<TrustedKvPermission, PM>::state_recovery_idempotent(initial_jv.durable_state,
+                                                                          initial_jv.constants),
             0 <= free_list_pos < iv.free_list.len(),
             iv.free_list[free_list_pos] == row_addr,
             sm.table.validate_row_addr(row_addr),
-            sm.table.end <= initial_durable_state.len(),
+            sm.table.end <= initial_jv.durable_state.len(),
             forall|s: Seq<u8>| Self::state_equivalent_for_me_specific(s, iv.durable_list_addrs,
-                                                                 initial_durable_state, constants, sm)
+                                                                 initial_jv.durable_state, initial_jv.constants, sm)
                 ==> #[trigger] perm.check_permission(s),
         ensures
-            forall|current_durable_state: Seq<u8>, current_read_state: Seq<u8>,
-              new_durable_state: Seq<u8>, new_read_state: Seq<u8>,
-              start: int, end: int| {
-                &&& #[trigger] seqs_match_except_in_range(current_durable_state, new_durable_state, start, end)
-                &&& #[trigger] seqs_match_except_in_range(current_read_state, new_read_state, start, end)
+            forall|current_durable_state: Seq<u8>, new_durable_state: Seq<u8>, start: int, end: int|
+                #![trigger seqs_match_except_in_range(current_durable_state, new_durable_state, start, end)]
+            {
+                &&& seqs_match_except_in_range(current_durable_state, new_durable_state, start, end)
                 &&& Self::state_equivalent_for_me_specific(current_durable_state, iv.durable_list_addrs,
-                                                         initial_durable_state, constants, sm)
+                                                         initial_jv.durable_state, initial_jv.constants, sm)
                 &&& iv.corresponds_to_durable_state(current_durable_state, sm)
-                &&& iv.corresponds_to_durable_state(current_read_state, sm)
                 &&& row_addr <= start <= end <= row_addr + sm.table.row_size
-                &&& Journal::<TrustedKvPermission, PM>::state_recovery_idempotent(new_durable_state, constants)
+                &&& Journal::<TrustedKvPermission, PM>::state_recovery_idempotent(new_durable_state,
+                                                                                initial_jv.constants)
             } ==> {
                 &&& Self::state_equivalent_for_me_specific(new_durable_state, iv.durable_list_addrs,
-                                                         initial_durable_state, constants, sm)
+                                                         initial_jv.durable_state, initial_jv.constants, sm)
                 &&& iv.corresponds_to_durable_state(new_durable_state, sm)
-                &&& iv.corresponds_to_durable_state(new_read_state, sm)
                 &&& perm.check_permission(new_durable_state)
             },
-    {
-        let list_addrs = iv.durable_list_addrs;
-        assert forall|current_durable_state: Seq<u8>, current_read_state: Seq<u8>,
-                 new_durable_state: Seq<u8>, new_read_state: Seq<u8>,
-                 start: int, end: int| {
-                &&& #[trigger] seqs_match_except_in_range(current_durable_state, new_durable_state, start, end)
-                &&& #[trigger] seqs_match_except_in_range(current_read_state, new_read_state, start, end)
-                &&& Self::state_equivalent_for_me_specific(current_durable_state, iv.durable_list_addrs,
-                                                         initial_durable_state, constants, sm)
-                &&& iv.corresponds_to_durable_state(current_durable_state, sm)
+
+            forall|current_read_state: Seq<u8>, start: int, bytes: Seq<u8>|
+                #![trigger update_bytes(current_read_state, start, bytes)]
+            {
+                let new_read_state = update_bytes(current_read_state, start, bytes);
+                &&& seqs_match_except_in_range(initial_jv.read_state, current_read_state, sm.start() as int,
+                                             sm.end() as int)
                 &&& iv.corresponds_to_durable_state(current_read_state, sm)
+                &&& row_addr <= start
+                &&& start + bytes.len() <= row_addr + sm.table.row_size
+            } ==> {
+                let new_read_state = update_bytes(current_read_state, start, bytes);
+                &&& iv.corresponds_to_durable_state(new_read_state, sm)
+                &&& seqs_match_except_in_range(initial_jv.read_state, new_read_state, sm.start() as int, sm.end() as int)
+            },
+    {
+        assert forall|current_durable_state: Seq<u8>, new_durable_state: Seq<u8>, start: int, end: int|
+                #![trigger seqs_match_except_in_range(current_durable_state, new_durable_state, start, end)]
+            {
+                &&& seqs_match_except_in_range(current_durable_state, new_durable_state, start, end)
+                &&& Self::state_equivalent_for_me_specific(current_durable_state, iv.durable_list_addrs,
+                                                         initial_jv.durable_state, initial_jv.constants, sm)
+                &&& iv.corresponds_to_durable_state(current_durable_state, sm)
                 &&& row_addr <= start <= end <= row_addr + sm.table.row_size
-                &&& Journal::<TrustedKvPermission, PM>::state_recovery_idempotent(new_durable_state, constants)
+                &&& Journal::<TrustedKvPermission, PM>::state_recovery_idempotent(new_durable_state,
+                                                                                initial_jv.constants)
             } implies {
                 &&& Self::state_equivalent_for_me_specific(new_durable_state, iv.durable_list_addrs,
-                                                         initial_durable_state, constants, sm)
+                                                         initial_jv.durable_state, initial_jv.constants, sm)
                 &&& iv.corresponds_to_durable_state(new_durable_state, sm)
-                &&& iv.corresponds_to_durable_state(new_read_state, sm)
                 &&& perm.check_permission(new_durable_state)
             } by {
             broadcast use group_validate_row_addr;
             broadcast use broadcast_seqs_match_in_range_can_narrow_range;
-            Self::lemma_writing_to_free_slot_doesnt_change_recovery(iv, current_durable_state, new_durable_state, sm,
-                                                                    free_list_pos, row_addr, start, end);
-            Self::lemma_writing_to_free_slot_doesnt_change_recovery(iv, current_read_state, new_read_state, sm,
-                                                                    free_list_pos, row_addr, start, end);
+            Self::lemma_writing_to_free_slot_doesnt_change_recovery(
+                iv, current_durable_state, new_durable_state, sm, free_list_pos, row_addr, start, end
+            );
+        }
+
+        assert forall|current_read_state: Seq<u8>, start: int, bytes: Seq<u8>|
+                #![trigger update_bytes(current_read_state, start, bytes)]
+            {
+                let new_read_state = update_bytes(current_read_state, start, bytes);
+                &&& seqs_match_except_in_range(initial_jv.read_state, current_read_state, sm.start() as int,
+                                             sm.end() as int)
+                &&& iv.corresponds_to_durable_state(current_read_state, sm)
+                &&& row_addr <= start
+                &&& start + bytes.len() <= row_addr + sm.table.row_size
+            } implies {
+                let new_read_state = update_bytes(current_read_state, start, bytes);
+                &&& iv.corresponds_to_durable_state(new_read_state, sm)
+                &&& seqs_match_except_in_range(initial_jv.read_state, new_read_state, sm.start() as int, sm.end() as int)
+            } by {
+            broadcast use group_validate_row_addr;
+            broadcast use broadcast_seqs_match_in_range_can_narrow_range;
+            broadcast use broadcast_update_bytes_effect;
+            let new_read_state = update_bytes(current_read_state, start, bytes);
+            Self::lemma_writing_to_free_slot_doesnt_change_recovery(
+                iv, current_read_state, new_read_state, sm, free_list_pos, row_addr, start, start + bytes.len()
+            );
         }
     }
 
@@ -344,10 +375,8 @@ impl<PM, L> ListTable<PM, L>
             if self.free_list@.len() > 0 {
                 Self::lemma_writing_to_free_slot_has_permission_later_forall(
                     self.internal_view(),
-                    journal@.durable_state,
-                    journal@.read_state,
+                    journal@,
                     self.sm,
-                    journal@.constants,
                     self.free_list@.len() - 1,
                     self.free_list@.last(),
                     perm
