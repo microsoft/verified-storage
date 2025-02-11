dadf54048e25ef3b7e02363f7b8816a39304ce60
Jay Lorch <lorch@microsoft.com>
2025-02-11 15:53:59 -0800

    Trim actions

--- storage_node/src/kv2/lists/trim_v.rs
+++ storage_node/src/kv2/lists/trim_v.rs
@@ -240,22 +240,201 @@ impl<L> ListTableInternalView<L>
     }
 }
 
+enum TrimAction
+{
+    Delete,
+    Modify{ pending_deallocations: Vec<u64>, new_head: u64 },
+    AdvanceHead{ pending_deallocations: Vec<u64>, new_head: u64 },
+    Drain{ pending_deallocations: Vec<u64> },
+}
+
+impl TrimAction
+{
+    pub(super) open spec fn applicable<L>(self, iv: ListTableInternalView<L>, list_addr: u64, trim_length: int) -> bool
+        where
+            L: PmCopy + LogicalRange + Sized + std::fmt::Debug,
+        recommends
+            iv.tentative_mapping.list_info.contains_key(list_addr),
+    {
+        let new_iv = iv.trim(list_addr, trim_length);
+        match self {
+            TrimAction::Delete => iv.tentative_mapping.list_info[list_addr].len() == trim_length,
+            TrimAction::Modify{ pending_deallocations, new_head } => {
+                &&& iv.m.contains_key(list_addr)
+                &&& iv.m[list_addr] is Durable
+                &&& pending_deallocations@ == iv.tentative_mapping.list_info[list_addr].take(trim_length)
+                &&& new_head == iv.tentative_mapping.list_info[list_addr][trim_length]
+            },
+            TrimAction::AdvanceHead{ pending_deallocations, new_head } => {
+                &&& iv.m.contains_key(list_addr)
+                &&& new_head == iv.tentative_mapping.list_info[list_addr][trim_length]
+                &&& pending_deallocations@ == iv.tentative_mapping.list_info[list_addr].take(trim_length)
+                &&& match iv.m[list_addr] {
+                    ListTableEntryView::Modified{ entry, addrs, .. } => entry.length - trim_length > addrs.len(),
+                    _ => false,
+                }
+            },
+            TrimAction::Drain{ pending_deallocations } => {
+                &&& iv.m.contains_key(list_addr)
+                &&& match iv.m[list_addr] {
+                    ListTableEntryView::Modified{ entry, addrs, .. } => {
+                        &&& entry.length - trim_length <= addrs.len()
+                        &&& pending_deallocations@ ==
+                            iv.tentative_mapping.list_info[list_addr].take(entry.length - addrs.len())
+                    },
+                    _ => false,
+                }
+            },
+        }
+    }
+}
+
 impl<PM, L> ListTable<PM, L>
     where
         PM: PersistentMemoryRegion,
         L: PmCopy + LogicalRange + Sized + std::fmt::Debug,
 {
+    exec fn get_addresses_to_trim_case_durable(
+        &self,
+        list_addr: u64,
+        trim_length: usize,
+        entry: &ListTableDurableEntry,
+        journal: &Journal<TrustedKvPermission, PM>,
+    ) -> (result: Result<(Vec<u64>, u64), KvError>)
+        requires
+            self.valid(journal@),
+            journal.valid(),
+            self@.tentative.is_some(),
+            self@.tentative.unwrap().m.contains_key(list_addr),
+            self.m@.contains_key(list_addr),
+            self.m@[list_addr] is Durable,
+            entry == self.m@[list_addr]->Durable_entry,
+            trim_length < entry.length,
+        ensures
+            match result {
+                Ok((addrs, new_head)) => {
+                    &&& addrs@ == self.tentative_mapping@.list_info[list_addr].take(trim_length as int)
+                    &&& new_head == self.tentative_mapping@.list_info[list_addr][trim_length as int]
+                },
+                Err(KvError::CRCMismatch) => !journal@.pm_constants.impervious_to_corruption(),
+                _ => false,
+            }
+    {
+        let mut current_addr = list_addr;
+        let mut result = Vec::<u64>::new();
+        let ghost addrs = self.durable_mapping@.list_info[list_addr];
+        let pm = journal.get_pm_region_ref();
+
+        assert(addrs.take(0) =~= Seq::<u64>::empty());
+        assert(list_addr != 0) by {
+            broadcast use group_validate_row_addr;
+        }
+        
+        for current_pos in 0..trim_length
+            invariant
+                trim_length < addrs.len(),
+                current_addr == addrs[current_pos as int],
+                current_addr != 0,
+                result@ == addrs.take(current_pos as int),
+                self.valid(journal@),
+                journal.valid(),
+                self.durable_mapping@.list_info.contains_key(list_addr),
+                addrs == self.durable_mapping@.list_info[list_addr],
+                pm.inv(),
+                pm@.read_state == journal@.read_state,
+                pm.constants() == journal@.pm_constants,
+            decreases
+                addrs.len() - result@.len(),
+        {
+            proof {
+                broadcast use group_validate_row_addr;
+                broadcast use pmcopy_axioms;
+            }
+
+            assert(addrs.take(current_pos as int).push(current_addr) =~= addrs.take(current_pos + 1));
+            result.push(current_addr);
+
+            let next_addr = current_addr + self.sm.row_next_start;
+            let next_crc_addr = next_addr + size_of::<u64>() as u64;
+            current_addr = match exec_recover_object::<PM, u64>(pm, next_addr, next_crc_addr) {
+                Some(n) => n,
+                None => { return Err(KvError::CRCMismatch); },
+            };
+        }
+        
+        assert(self.tentative_mapping@.list_info[list_addr] == self.durable_mapping@.list_info[list_addr]);
+        Ok((result, current_addr))
+    }
+
+    exec fn determine_action(
+        &self,
+        list_addr: u64,
+        trim_length: usize,
+        journal: &Journal<TrustedKvPermission, PM>,
+    ) -> (result: Result<TrimAction, KvError>)
+        requires
+            self.valid(journal@),
+            journal.valid(),
+            self@.tentative is Some,
+            self@.tentative.unwrap().m.contains_key(list_addr),
+            0 < trim_length,
+        ensures
+            match result {
+                Ok(action) => action.applicable(self.internal_view(), list_addr, trim_length as int),
+                Err(KvError::CRCMismatch) => !journal@.pm_constants.impervious_to_corruption(),
+                Err(KvError::IndexOutOfRange{ upper_bound }) => {
+                    &&& upper_bound == self@.tentative.unwrap().m[list_addr].len()
+                    &&& upper_bound < trim_length
+                },
+                _ => false,
+            }
+    {
+        proof {
+            self.lemma_valid_implications(journal@);
+            journal.lemma_valid_implications();
+            broadcast use group_hash_axioms;
+        }
+
+        match self.m.get(&list_addr) {
+            None => {
+                assert(false);
+                Err(KvError::InternalError)
+            },
+            Some(ListTableEntry::<L>::Durable{ ref entry }) => {
+                if entry.length < trim_length {
+                    Err(KvError::IndexOutOfRange{ upper_bound: entry.length })
+                }
+                else if entry.length == trim_length {
+                    Ok(TrimAction::Delete)
+                }
+                else {
+                    match self.get_addresses_to_trim_case_durable(list_addr, trim_length, entry, journal) {
+                        Ok((pending_deallocations, new_head)) =>
+                            Ok(TrimAction::Modify{ pending_deallocations, new_head }),
+                        Err(KvError::CRCMismatch) => Err(KvError::CRCMismatch),
+                        _ => { assert(false); Err(KvError::InternalError) },
+                    }
+                }
+            }
+            Some(ListTableEntry::<L>::Modified{ ref durable_head, ref entry, ref elements, .. }) => {
+                assume(false);
+                Err(KvError::NotImplemented)
+            }
+        }
+    }
+
     pub exec fn trim(
         &mut self,
-        row_addr: u64,
+        list_addr: u64,
         trim_length: usize,
         journal: &mut Journal<TrustedKvPermission, PM>,
         Tracked(perm): Tracked<&TrustedKvPermission>,
     ) -> (result: Result<u64, KvError>)
         requires
             old(self).valid(old(journal)@),
+            old(journal).valid(),
             old(self)@.tentative is Some,
-            old(self)@.tentative.unwrap().m.contains_key(row_addr),
+            old(self)@.tentative.unwrap().m.contains_key(list_addr),
             forall|s: Seq<u8>| old(self).state_equivalent_for_me(s, old(journal)@) ==> #[trigger] perm.check_permission(s),
             0 < trim_length,
         ensures
@@ -263,25 +442,25 @@ impl<PM, L> ListTable<PM, L>
             journal.valid(),
             journal@.matches_except_in_range(old(journal)@, self@.sm.start() as int, self@.sm.end() as int),
             match result {
-                Ok(new_row_addr) => {
-                    let old_list = old(self)@.tentative.unwrap().m[row_addr];
+                Ok(new_list_addr) => {
+                    let old_list = old(self)@.tentative.unwrap().m[list_addr];
                     &&& {
-                           ||| new_row_addr == 0
-                           ||| new_row_addr == row_addr
-                           ||| !old(self)@.tentative.unwrap().m.contains_key(new_row_addr)
+                           ||| new_list_addr == 0
+                           ||| new_list_addr == list_addr
+                           ||| !old(self)@.tentative.unwrap().m.contains_key(new_list_addr)
                     }
                     &&& trim_length <= old_list.len()
-                    &&& new_row_addr == 0 ==> old_list.skip(trim_length as int) == Seq::<L>::empty()
+                    &&& new_list_addr == 0 ==> old_list.skip(trim_length as int) == Seq::<L>::empty()
                     &&& self@ == (ListTableView {
-                        tentative: Some(old(self)@.tentative.unwrap().trim(row_addr, new_row_addr, trim_length as int)),
+                        tentative: Some(old(self)@.tentative.unwrap().trim(list_addr, new_list_addr, trim_length as int)),
                         ..old(self)@
                     })
-                    &&& self.validate_list_addr(new_row_addr)
+                    &&& self.validate_list_addr(new_list_addr)
                 },
                 Err(KvError::IndexOutOfRange{ upper_bound }) => {
                     &&& self@ == old(self)@
                     &&& trim_length > upper_bound
-                    &&& upper_bound == old(self)@.tentative.unwrap().m[row_addr].len()
+                    &&& upper_bound == old(self)@.tentative.unwrap().m[list_addr].len()
                 },
                 Err(KvError::OutOfSpace) => {
                     &&& self@ == (ListTableView {
@@ -299,24 +478,6 @@ impl<PM, L> ListTable<PM, L>
                 _ => false,
             }
     {
-        proof {
-            broadcast use group_hash_axioms;
-        }
-        /*
-
-        let result = match self.m.get(&list_addr) {
-            None => {
-                assert(false);
-                Err(KvError::InternalError)
-            },
-            Some(ListTableEntry::<L>::Durable{ ref entry }) =>
-                self.get_addresses_to_trim_case_durable(list_addr, trim_length, entry, journal),
-            Some(ListTableEntry::<L>::Modified{ ref durable_head, ref entry, ref elements, .. }) =>
-                self.get_addresses_to_trim_case_modified(list_addr, trim_length, Ghost(durable_head@), entry,
-                                                         elements, journal),
-        };
-        */
-
         assume(false);
         Err(KvError::NotImplemented)
     }
