f2350580d2064301c05451a68e54591978dca033
Jay Lorch <lorch@microsoft.com>
2025-02-18 15:54:03 -0800

    Use cfg verus_keep_ghost to enable standalone building of overflow_v

--- storage_node/src/common/overflow_v.rs
+++ storage_node/src/common/overflow_v.rs
@@ -43,8 +43,11 @@
 
 use builtin::*;
 use builtin_macros::*;
-use vstd::arithmetic::div_mod::*;
-use vstd::arithmetic::mul::*;
+#[cfg(verus_keep_ghost)]
+use vstd::arithmetic::div_mod::{lemma_div_is_ordered_by_denominator, lemma_div_plus_one, lemma_fundamental_div_mod,
+                                lemma_mod_division_less_than_divisor};
+#[cfg(verus_keep_ghost)]
+use vstd::arithmetic::mul::{lemma_mul_by_zero_is_zero, lemma_mul_inequality, lemma_mul_is_commutative};
 use vstd::prelude::*;
 
 verus! {
