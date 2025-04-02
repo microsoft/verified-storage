d87b80b2d8f2ed0006e8877fcddabc0fcc318abd
hayley-leblanc <hleblanc@utexas.edu>
2025-04-02 17:17:10 -0500

    Fix typo

--- evaluation/run_ycsb_mini.sh
+++ evaluation/run_ycsb_mini.sh
@@ -2,7 +2,7 @@
 
 export LD_LIBRARY_PATH=$PWD/ycsb_ffi/target/release:$PWD/viper_wrapper:$PWD/viper_deps/benchmark/build/src:$PWD/viper_deps/benchmark/include:$PWD/viper/benchmark
 
-numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db capybarakv --experiment_config config/mini_experiment_config.toml --capybarakv_config configs/mini_capybarakv_config.toml --workloads A
-numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db pmemrocksdb --experiment_config config/mini_experiment_config.toml --workloads A
-numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db redis --experiment_config config/mini_experiment_config.toml --workloads A
-numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db viper --experiment_config config/mini_experiment_config.toml --workloads A
+numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db capybarakv --experiment_config configs/mini_experiment_config.toml --capybarakv_config configs/mini_capybarakv_config.toml --workloads A
+numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db pmemrocksdb --experiment_config configs/mini_experiment_config.toml --workloads A
+numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db redis --experiment_config configs/mini_experiment_config.toml --workloads A
+numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db viper --experiment_config configs/mini_experiment_config.toml --workloads A
