abaa732ef2b46bb9912d5162603a075e89d81b67
hayley-leblanc <hleblanc@utexas.edu>
2025-04-03 14:48:49 -0500

    Add output to setup script

--- evaluation/setup.sh
+++ evaluation/setup.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+BOLD=$(tput bold)
+NC=$(tput sgr0)
+BG=${BOLD}${GREEN}
+
 # 1. Install Rust and automatically select default installation
+printf "${BOLD}Installing Rust...${NC}"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "$HOME/.cargo/env"
+printf "${BG}Done installing Rust!${NC}"
 
 # 2. Confirm that verified-storage crates build successfully
+printf "${BOLD}Building verified-storage crates...${NC}"
 cd ../deps_hack; cargo build --release
 cd ../pmsafe; cargo build --release
 cd ../storage_node/src; cargo build --release
+printf "${BG}Done building verified-storage crates!${NC}"
 
 # 3. Clone and build Verus
-cd ../../; git clone git@github.com:verus-lang/verus.git; cd verus/source/; source ../tools/activate; ./tools/get-z3.sh; vargo build --release
+printf "${BOLD}Cloning and building Verus...${NC}"
+cd ../../; git clone https://github.com/verus-lang/verus.git; cd verus/source/; source ../tools/activate; ./tools/get-z3.sh; vargo build --release
+printf "${BG}Done building Verus!${NC}"
 
 # 4. Confirm that CapybaraKV verifies
+printf "${BOLD}Verifying CapybaraKV...${NC}"
 cd ../storage_node/src/; ./verify.sh
+printf "${BG}Done verifying CapybaraKV!${NC}"
