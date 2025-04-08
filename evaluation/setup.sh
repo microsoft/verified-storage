eaad01b4b323deb2e6b2f20622ebc284a8147ffb
hayley-leblanc <hleblanc@utexas.edu>
2025-04-08 16:33:13 +0000

    Updated setup script

--- evaluation/setup.sh
+++ evaluation/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # exit on error
-set -e 
+set -e
 set -o pipefail
 
 PROJECT_DIR=`realpath $(pwd)/../..`
@@ -14,35 +14,44 @@ MAGENTA="\e[35m"
 BOLD=$(tput bold)
 NC="\e[0m"
 
+total_steps=11
+current_step=0
+
+step() {
+    printf "${MAGENTA}[${current_step}/${total_steps}]${NC} "
+    current_step=$((current_step+1))
+}
+
 LD_LIBRARY_PATH=$VERIF_STORAGE_DIR/evaluation/ycsb_ffi/target/release:$VERIF_STORAGE_DIR/evaluation/viper_wrapper:$VERIF_STORAGE_DIR/evaluation/viper_deps/benchmark/build/src:$VERIF_STORAGE_DIR/evaluation/viper_deps/benchmark/include:$VERIF_STORAGE_DIR/evaluation/viper/benchmark
 JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64/
 
 # 1. Install apt dependencies
 # TODO: is valgrind necessary?
-printf "${BOLD}${MAGENTA}Installing dependencies...${NC}\n"
+step; printf "${BOLD}${MAGENTA}Installing dependencies...${NC}\n"
 sudo apt update
 sudo apt -y install default-jdk default-jre libpmemobj-dev libsnappy-dev \
     pkg-config autoconf automake libtool libndctl-dev libdaxctl-dev libnuma-dev \
     daxctl libzstd-dev cmake build-essential liblz4-dev libpmempool-dev valgrind \
     python3-toml numactl llvm-dev libclang-dev clang libpmem1 libpmem-dev \
-    python3-pip python3-prettytable unzip
+    python3-pip python3-prettytable unzip tokei
 printf "${BOLD}${MAGENTA}Done installing dependencies!${NC}\n\n\n"
 
 # 2. Install Rust and automatically select default installation
-printf "${BOLD}${MAGENTA}Installing Rust...${NC}\n"
+step; printf "${BOLD}${MAGENTA}Installing Rust...${NC}\n"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "$HOME/.cargo/env"
 printf "${BOLD}${MAGENTA}Done installing Rust!${NC}\n\n\n"
 
+
 # 3. Confirm that verified-storage crates build successfully
-printf "${BOLD}${MAGENTA}Building verified-storage crates...${NC}\n"
+step; printf "${BOLD}${MAGENTA}Building verified-storage crates...${NC}\n"
 cd $VERIF_STORAGE_DIR/deps_hack; cargo build --release
 cd $VERIF_STORAGE_DIR/pmsafe; cargo build --release
 cd $VERIF_STORAGE_DIR/storage_node/src; cargo build --release
 printf "${BOLD}${MAGENTA}Done building verified-storage crates!${NC}\n\n\n"
 
 # 4. Clone and build Verus
-printf "${BOLD}${MAGENTA}Cloning and building Verus...${NC}\n"
+step; printf "${BOLD}${MAGENTA}Cloning and building Verus...${NC}\n"
 cd $PROJECT_DIR
 if [ -d verus ]; 
 then 
@@ -56,7 +65,7 @@ then
     printf "${BOLD}${MAGENTA}Z3 is not present, downloading...${NC}\n"
     ./tools/get-z3.sh
 fi
-printf "${BOLD}${MAGENTA}Building Verus...${NC}\n"
+step; printf "${BOLD}${MAGENTA}Building Verus...${NC}\n"
 /bin/bash -c "source ../tools/activate; vargo build --release"
 printf "${BOLD}${MAGENTA}Done building Verus!${NC}\n\n\n"
 
@@ -69,7 +78,7 @@ grep -qxF "${path_verus}" $HOME/.bashrc || echo $path_verus >> $HOME/.bashrc
 # printf "${BOLD}${MAGENTA}Done verifying CapybaraKV!${NC}\n\n\n"
 
 # 6. Download Maven for YCSB
-printf "${BOLD}${MAGENTA}Downloading Maven...${NC}\n"
+step; printf "${BOLD}${MAGENTA}Downloading Maven...${NC}\n"
 cd $PROJECT_DIR
 if [ -d maven ];
 then
@@ -83,27 +92,28 @@ fi
 path_maven="export PATH=\$PATH:${PROJECT_DIR}/maven/bin"
 grep -qxF "${path_maven}" $HOME/.bashrc || echo $path_maven >> $HOME/.bashrc
 printf "${BOLD}${MAGENTA}Done downloading Maven${NC}\n\n\n"
+source ~/.bashrc
 
 #7. Build YCSB FFI layer
-printf "${BOLD}${MAGENTA}Building YCSB FFI layer for CapybaraKV...${NC}\n"
+step; printf "${BOLD}${MAGENTA}Building YCSB FFI layer for CapybaraKV...${NC}\n"
 cd $VERIF_STORAGE_DIR/evaluation/ycsb_ffi
 cargo build --release
 printf "${BOLD}${MAGENTA}Done building YCSB FFI layer for CapybaraKV!${NC}\n\n\n"
 
 # 8. Building pmem-RocksDB
-printf "${BOLD}${MAGENTA}Building pmem-RocksDB...${NC}\n"
+step; printf "${BOLD}${MAGENTA}Building pmem-RocksDB...${NC}\n"
 cd $VERIF_STORAGE_DIR/evaluation/pmem-rocksdb
 make rocksdbjava ROCKSDB_ON_DCPMM=1 DISABLE_WARNING_AS_ERROR=true JAVA_HOME=$JAVA_HOME
 printf "${BOLD}${MAGENTA}Done building pmem-RocksDB!${NC}\n\n\n"
 
 # 9. Building pmem-Redis
-printf "${BOLD}${MAGENTA}Building pmem-redis...${NC}\n"
+step; printf "${BOLD}${MAGENTA}Building pmem-redis...${NC}\n"
 cd $VERIF_STORAGE_DIR/evaluation/pmem-redis
 make USE_NVM=yes
 printf "${BOLD}${MAGENTA}Done building pmem-redis!${NC}\n\n\n"
 
 # 10. Build YCSB bindings
-printf "${BOLD}${MAGENTA}Building YCSB bindings...${NC}\n"
+step; printf "${BOLD}${MAGENTA}Building YCSB bindings...${NC}\n"
 cd $VERIF_STORAGE_DIR/evaluation/YCSB
 mvn -pl site.ycsb:capybarakv-binding -am clean package
 mvn -pl site.ycsb:redis-binding -am clean package 
@@ -112,7 +122,7 @@ mvn -pl site.ycsb:viper-binding -am clean package
 printf "${BOLD}${MAGENTA}Done building YCSB bindings!${NC}\n\n\n"
 
 #11. Build Viper dependencies
-printf "${BOLD}${MAGENTA}Building Viper dependencies...${NC}\n"
+step; printf "${BOLD}${MAGENTA}Building Viper dependencies...${NC}\n"
 printf "Building libpmemobj-cpp\n"
 cd $VERIF_STORAGE_DIR/evaluation/viper_deps/libpmemobj-cpp
 mkdir -p build
@@ -129,7 +139,7 @@ sudo cmake --build "build" --config Release --target install
 printf "${BOLD}${MAGENTA}Done building Viper dependencies!${NC}\n\n\n"
 
 # 12. Build Viper wrapper
-printf "${BOLD}${MAGENTA}Building Viper wrapper...${NC}\n"
+step; printf "${BOLD}${MAGENTA}Building Viper wrapper...${NC}\n"
 cd $VERIF_STORAGE_DIR/evaluation/viper_wrapper
 make all JAVA_HOME=$JAVA_HOME
 printf "${BOLD}${MAGENTA}Done building Viper wrapper!${NC}\n\n\n"
