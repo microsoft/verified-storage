c4acadc08fde0d5c26384ae890b6c3bebacde85f
Jay Lorch <lorch@microsoft.com>
2025-04-15 15:30:28 -0700

    Add comments to allow auditing of impl_t.rs

--- storage_node/src/kv2/impl_t.rs
+++ storage_node/src/kv2/impl_t.rs
@@ -1,12 +1,63 @@
-//! This file contains the public interface of the paged key-value store.
-//! The methods offered by this file should match the mocks.
-//! The key-value store itself should be as generic as possible, not
-//! restricted to particular data structures.
-//! We define legal crash states at this level and pass them
-//! to the untrusted implementation, which passes them along
-//! to untrusted components.
+//! This file implements `KvStoreImpl`, a single-threaded key-value
+//! store also known as CapybaraKV. It uses PoWER to enforce crash
+//! consistency, by limiting the set of allowable crash states in the
+//! middle of operations.
 //!
-//! This file is unverified and should be tested/audited for correctness.
+//! The implementation of `KvStoreImpl` relies on an untrusted KV
+//! store `UntrustedKvStoreImpl` to do the actual work of maintaining
+//! KV state in memory and on disk. Because that code is verified, we
+//! know that its operations are functionally correct. Because this
+//! file wraps calls to that store in PoWER wrappers, we know that it
+//! must be crash-consistent as well.
+//!
+//! This file is unverified and should be tested/audited for
+//! correctness, most notably to verify that the PoWER wrappers indeed
+//! enforce crash consistency.
+//!
+//! The API for `KvStoreImpl` is transactional. That is, all mutating
+//! operations are tentative in that they are applied only to the
+//! current tentative state and are undone in the event of a crash.
+//! The only way to make the tentative state durable is by calling
+//! `commit`. One can undo all the tentative operations since the last
+//! commit by calling `abort`. Read-only operations read from the
+//! current tentative state.
+//!
+//! A `KvStoreImpl<K, I, L>` maps each key of type `K` to a value
+//! consisting of an item of type `I` and an append-only list of
+//! elements of type `L`. Thus, while key, item, and element types are
+//! fixed-size, lists can grow arbitrarily long, permitting
+//! arbitrary-sized values. Each of the types `K`, `I`, and `L` must
+//! implement `PmCopy`, permitting their storage on persistent memory;
+//! one can do this with `#[repr(C)]` and `#[derive(PmCopy, Copy)]`.
+//!
+//! Supported read-only operations are as follows:
+//!
+//! * `read_item`
+//! * `read_item_and_list`
+//! * `read_list_element_at_index`
+//! * `get_list_length`
+//! * `get_keys`
+//!
+//! Supported mutating operations are as follows. To make clear that
+//! these operations are all tentative, the functions invoking them
+//! all start with `tentatively_`, e.g., `tentatively_create`:
+//!
+//! * `create`
+//! * `update_item`
+//! * `delete`
+//! * `append_to_list`
+//! * `append_to_list_and_update_item`
+//! * `update_list_element_at_index`
+//! * `update_list_element_at_index_and_item`
+//! * `trim_list`
+//! * `trim_list_and_update_item`
+//!
+//! To create a new KV store on persistent memory, overwriting
+//! whatever is already there, one uses `setup`. To create a
+//! `KvStoreImpl` using the state on persistent memory (either
+//! immediately after setup or after running for a while then
+//! crashing), one uses `start`.
+//!
 
 #![allow(unused_imports)]
 #![cfg_attr(verus_keep_ghost, verus::trusted)]
@@ -24,6 +75,11 @@ use super::inv_v::*;
 use super::spec_t::*;
 
 verus! {
+
+// This structure represents a permission to transition the state of
+// the persistent storage from one state (represented as a sequence of
+// `u8`) to another. Its fields aren't `pub`, so no code outside this
+// trusted file can create such a permission.
 
 pub struct TrustedKvPermission
 {
@@ -50,12 +106,27 @@ impl CheckPermission<Seq<u8>> for TrustedKvPermission
         true
     }
 
+    // The trait `CheckPermission` is general enough to support two
+    // scenarios: (1) Audited, trusted code creates the permission,
+    // and we rely on the auditor to ensure that this permission
+    // implies that the transition is permissible. That is, we rely on
+    // someone reading this file carefully to decide whether the
+    // permission is reasonable to grant. (2) The permission does
+    // something useful when code uses it to effect a write by calling
+    // its `apply` function. In this file, we use scenario (1), so we
+    // can just have `apply` be an assumed-correct function.
+
     proof fn apply(tracked self, tracked credit: vstd::invariant::OpenInvariantCredit, tracked r: &mut GhostVarAuth<Seq<u8>>, new_state: Seq<u8>) -> (tracked result: Self::Completion)
     {
         admit();
         ()
     }
 }
+
+// This structure is a factory that can generate an arbitrary number
+// of copies of a certain transition permission. We use it to give the
+// untrusted code blanket permission to perform any storage
+// transitions that are functionally idempotent.
 
 pub struct TrustedKvPermissionFactory
 {
@@ -97,6 +168,11 @@ impl PermissionFactory<Seq<u8>> for TrustedKvPermissionFactory
         self.powerpm_id
     }
 }
+
+// This is the trusted structure that exposes the KV store API.
+// Internally, it uses `UntrustedKvStoreImpl` for its functionality
+// and uses PoWER-based permissions to restrict how that untrusted KV
+// store can crash.
 
 #[verifier::reject_recursive_types(K)]
 #[verifier::reject_recursive_types(I)]
@@ -165,6 +241,10 @@ where
         UntrustedKvStoreImpl::<TrustedKvPermissionFactory, PM, K, I, L>::space_needed_for_setup(ps)
     }
 
+    // This function sets up a persistent-memory (PM) region to serve
+    // as storage for a KV store. It writes initial contents there
+    // representing an empty KV store.
+
     pub exec fn setup(pm: &mut PM, ps: &SetupParameters) -> (result: Result<(), KvError>)
         requires 
             old(pm).inv(),
@@ -185,6 +265,11 @@ where
     {
         UntrustedKvStoreImpl::<TrustedKvPermissionFactory, PM, K, I, L>::setup(pm, ps)
     }
+
+    // This function restores the in-memory state of a KV store from
+    // PM after a crash, or immediately after setup. It assumes the
+    // state is recoverable because every other function ensures
+    // that's the case even during crashes.
 
     pub exec fn start(pm: PM, kvstore_id: u128) -> (result: Result<Self, KvError>)
         requires 
@@ -219,6 +304,17 @@ where
     {
         let (mut powerpm, _) = PoWERPersistentMemoryRegion::new(pm);
         powerpm.flush(); // ensure there are no outstanding writes
+
+        // At startup, we grant a blanket permission for as long as
+        // the program runs, represented by a factory that can
+        // generate the same permission an arbitrary number of times.
+        // That permission allows any write to storage as long as it
+        // can't change the post-recovery state, even if that write
+        // only completes partially due to a crash. In other words, a
+        // transition from state `s1` to `s2` is allowable only if
+        // both those recover to the same abstract state according to
+        // the untrusted recovery specification.
+
         let ghost state = UntrustedKvStoreImpl::<TrustedKvPermissionFactory,
                                                  PM, K, I, L>::recover(powerpm@.durable_state).unwrap();
         let ghost is_transition_allowable: spec_fn(Seq<u8>, Seq<u8>) -> bool = |s1: Seq<u8>, s2: Seq<u8>| {
@@ -403,6 +499,16 @@ where
                 Err(_) => false,
             },
     {
+        // To effect commit, we need to pass the untrusted code
+        // permission to make a transition from one storage state to
+        // another. Specifically, we give it permission to make a
+        // single transition from the durable state (the one that
+        // would result from a crash right now) to the tentative state
+        // (the one that should result from this commit). We also need
+        // to give it permission to stay in the durable state, to
+        // allow for the possibility that the committing write crashes
+        // before anything changes on the storage.
+
         let ghost is_transition_allowable: spec_fn(Seq<u8>, Seq<u8>) -> bool = |s1: Seq<u8>, s2: Seq<u8>| ({
             &&& UntrustedKvStoreImpl::<TrustedKvPermissionFactory, PM, K, I, L>::recover(s1) ==
                 Some(RecoveredKvStore::<K, I, L>{ ps: self@.ps, kv: self@.durable })
