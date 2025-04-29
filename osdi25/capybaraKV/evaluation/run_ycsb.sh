ff9accdb1e0a35a807c68de432719627654227ce
hayley-leblanc <hleblanc@utexas.edu>
2025-04-29 19:01:44 +0000

    Only run required workloads in run_ycsb.sh

--- osdi25/capybaraKV/evaluation/run_ycsb.sh
+++ osdi25/capybaraKV/evaluation/run_ycsb.sh
@@ -9,14 +9,14 @@ numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db viper --experiment_
 numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db capybarakv --experiment_config configs/experiment_config_mem_storage.toml --capybarakv_config configs/capybarakv_config_mem_storage.toml --workloads A
 
 numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db capybarakv --experiment_config configs/experiment_config1.toml 
-numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db pmemrocksdb --experiment_config configs/experiment_config1.toml 
-numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db redis --experiment_config configs/experiment_config1.toml 
-numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db viper --experiment_config configs/experiment_config1.toml
+numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db pmemrocksdb --experiment_config configs/experiment_config1.toml --workloads A,B,C,D,F,X
+numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db redis --experiment_config configs/experiment_config1.toml --workloads A,B,C,D,F,X
+numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db viper --experiment_config configs/experiment_config1.toml--workloads A,B,C,D,F,X
 
 numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db capybarakv --experiment_config configs/experiment_config16.toml 
-numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db pmemrocksdb --experiment_config configs/experiment_config16.toml 
-numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db redis --experiment_config configs/experiment_config16.toml 
-numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db viper --experiment_config configs/experiment_config16.toml
+numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db pmemrocksdb --experiment_config configs/experiment_config16.toml --workloads A,B,C,D,F,X
+numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db redis --experiment_config configs/experiment_config16.toml --workloads A,B,C,D,F,X
+numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db viper --experiment_config configs/experiment_config16.toml --workloads A,B,C,D,F,X
 
 numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db capybarakv --experiment_config configs/experiment_config8.toml
 numactl --membind 0 --cpunodebind 0 python3 run_ycsb.py --db capybarakv --experiment_config configs/experiment_config4.toml
